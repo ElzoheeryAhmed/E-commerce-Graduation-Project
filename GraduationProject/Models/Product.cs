5b72bc2a2c56d8d340b541390a2b4da874573bc3
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-17 14:11:35 +0200

    Update Product.cs

--- GraduationProject/Models/Product.cs
+++ GraduationProject/Models/Product.cs
@@ -1,4 +1,4 @@
-﻿using GraduationProject.Models.ModelEnums;
+using GraduationProject.Models.ModelEnums;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -9,62 +9,34 @@ namespace GraduationProject.Models
 		[Key]
     	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public string Id { get; set; } // Guid.NewGuid().ToString();
-		// public string ProductId { get; set; }			// 10
 		
 		[Required]
-		public string Title { get; set; }					// 3
+		public string Title { get; set; }
 		
 		[Required]
-		public string Description { get; set; }				// 2
+		public string? Description { get; set; }
 		
 		[Required]
     	[Range(0.01, 100_000_0000.0, ErrorMessage = "Price must be greater than 0. Max price allowed is $1 Million.")]
-		public decimal Price { get; set; }					// 9
+		public decimal Price { get; set; }
 		
 		[Range(0.0, 100_000_0000.0, ErrorMessage = "Discount must be greater than or equal to 0. Max discount allowed is $1 Million.")]
-		public decimal Discount { get; set; }               // ---
+		public decimal Discount { get; set; }
 		
-		public ProductStatus Status { get; set; }			// ---
+		public ProductStatus Status { get; set; }
 		
-		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-		public DateTime DateAdded { get; set; }				// ---
+		public DateTime DateAdded { get; set; }
 
-		public string? Brand { get; set; }					// 5
-		public int VoteCount { get; set; }					// 12
-		public double VoteAverage { get; set; }				// 13
-		// public string MainCategory { get; set; }			// 8
-		public string Categories { get; set; }              // 1  (Separated by ' || ')
-		public string Features { get; set; }				// 6  (Separated by ' || ')
-		public string HighResImageURLs { get; set; }		// 11 (Separated by ' || ')
+		public int VoteCount { get; set; }
+		public double VoteAverage { get; set; }
+		// public string MainCategory { get; set; }
+		public string Features { get; set; }
+		public string HighResImageURLs { get; set; }
 		
-		public ICollection<Rating> Ratings { get; set; }
-		public ICollection<Review> Reviews { get; set; }
-		
-		//public List<int>? AlsoBoughtProductIds { get; set; }// 4
-		//public List<int>? AlsoViewedProductIds { get; set; }// 7
-		
-		// TODO: Add a new field `Overall_Rating`.
-		// TODO: Add a new field `TimesAccessed`. When this time reaches, like, 100 times, a new access triggers the update of the overall_rating of the product.
-		
-		public Product()
-		{
-			this.Ratings = new List<Rating>();
-			this.Reviews = new List<Review>();
-		}
+		public int BrandId { get; set; }
+		public Brand Brand { get; set; }
+		public List<ProductCategory> ProductCategories { get; set; } = new List<ProductCategory>();
+		public ICollection<Rating> Ratings { get; set; } = new HashSet <Rating>();
+		public ICollection<Review> Reviews { get; set; } = new HashSet <Review>();
 	}
 }
-
-
-// category
-// description
-// title
-// also_buy
-// brand
-// feature
-// also_view
-// main_cat
-// price
-// asin
-// imageURLHighRes
-// vote_count
-// vote_average
