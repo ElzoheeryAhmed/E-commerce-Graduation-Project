4f48d8e865952279e028b94ebd893aee425942e9
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-17 14:18:08 +0200

    Adding new repository and context objects.

--- GraduationProject/Repository/UnitOfWork.cs
+++ GraduationProject/Repository/UnitOfWork.cs
@@ -1,4 +1,4 @@
-﻿using GraduationProject.Data;
+using GraduationProject.Data;
 using GraduationProject.IRepository;
 using GraduationProject.Models;
 
@@ -8,6 +8,10 @@ namespace GraduationProject.Repository
 	{
 		private readonly AppDbContext _context;
 		private IGenericRepository<Product> _products;
+        private IGenericRepository<ProductCategory> _productCategories;
+		private IGenericRepository<ProductCategoryJoin> _prodcutCategoryJoins;
+        private IGenericRepository<Brand> _brands;
+        private GenericRepository<ProductUpdate> _productUpdates;
 		private IGenericRepository<Rating> _ratings;
         private GenericRepository<Review> _reviews;
 
@@ -15,14 +19,24 @@ namespace GraduationProject.Repository
 		{
 			_context = context;
 			_products = new GenericRepository<Product>(_context);
+			_productCategories = new GenericRepository<ProductCategory>(_context);
+			_prodcutCategoryJoins = new GenericRepository<ProductCategoryJoin>(_context);
+			_brands = new GenericRepository<Brand>(_context);
+			_productUpdates = new GenericRepository<ProductUpdate>(_context);
 			_ratings = new GenericRepository<Rating>(_context);
 			_reviews = new GenericRepository<Review>(_context);
 		}
 
 		// Encapsulation of _products and _ratings.
 		public IGenericRepository<Product> Products => _products;
+        public IGenericRepository<ProductCategory> ProductCategories => _productCategories;
+        public IGenericRepository<Brand> Brands => _brands;
+        public IGenericRepository<ProductUpdate> ProductUpdates => _productUpdates;
         public IGenericRepository<Rating> Ratings => _ratings;
         public IGenericRepository<Review> Reviews => _reviews;
+        public IGenericRepository<ProductCategoryJoin> ProductCategoryJoins => _prodcutCategoryJoins;
+
+        public AppDbContext Context => _context;
 
 
         public void Dispose()
