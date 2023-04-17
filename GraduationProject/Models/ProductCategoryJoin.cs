1d99dab11c0f2699f4087f148ab1b3c112dd5df2
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-17 14:07:57 +0200

    Add files via upload

---
+++ GraduationProject/Models/ProductCategoryJoin.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraduationProject.Models
+{
+    public class ProductCategoryJoin
+    {
+        public string ProductId { get; set; }
+        public int ProductCategoryId { get; set; }
+        // public Product Product { get; set; }
+        // public ProductCategory ProductCategory { get; set; }
+    }
+}
