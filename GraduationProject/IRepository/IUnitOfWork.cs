9c77f2f1a54568bc687442e28d397bd0211eac2b
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-17 13:55:37 +0200

    Adding new repository and context objects.

--- GraduationProject/IRepository/IUnitOfWork.cs
+++ GraduationProject/IRepository/IUnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using GraduationProject.Models;
+using GraduationProject.Data;
+using GraduationProject.Models;
 
 namespace GraduationProject.IRepository
 {
@@ -6,7 +7,12 @@ namespace GraduationProject.IRepository
 	// By implementing the IDisposable interface, the unit of work can expose a Dispose() method that can be used to release any unmanaged resources. It can be called manually or automatically when the unit of work is used in a using block.
 	public interface IUnitOfWork : IDisposable
 	{
+		AppDbContext Context { get; }
 		IGenericRepository<Product> Products { get; }
+		IGenericRepository<ProductCategory> ProductCategories { get; }
+		IGenericRepository<ProductCategoryJoin> ProductCategoryJoins { get; }
+		IGenericRepository<Brand> Brands { get; }
+		IGenericRepository<ProductUpdate> ProductUpdates { get; }
 		IGenericRepository<Rating> Ratings { get; }
 		IGenericRepository<Review> Reviews { get; }
 		Task Save();
