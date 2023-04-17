653cde210de17ae72e68e1318a39d6bc8bda23ea
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-17 13:40:06 +0200

    Adding Get pages, Post, Put, and Delete endpoints.

--- GraduationProject/Controllers/RatingController.cs
+++ GraduationProject/Controllers/RatingController.cs
@@ -2,7 +2,6 @@ using AutoMapper;
 using GraduationProject.IRepository;
 using GraduationProject.Models;
 using GraduationProject.Models.Dto;
-using GraduationProject.Utils;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -28,12 +27,9 @@ namespace GraduationProject.Controllers
         [HttpGet]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        [Route("ratings/getAllRatings")]
-		public async Task<IActionResult> GetAllRatings()
-		{
-			try
-			{
-				var ratings = await _unitOfWork.Ratings.GetAllAsync();
+		public async Task<IActionResult> GerRatings([FromQuery] PagingFilter pagingFilter) {
+			try {
+				var ratings = await _unitOfWork.Ratings.GetPagedList(pagingFilter);
 
 				return Ok(_mapper.Map<IList<RatingDto>>(ratings));
 			}
@@ -44,40 +40,142 @@ namespace GraduationProject.Controllers
 			}
 		}
         
-        [HttpGet]
+        [HttpGet("{userId}/{productId}")]
+		[ActionName(nameof(GetRating))]
 		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
-		[Route("ratings/user/{userId}/product/{productId}")]
 		public async Task<IActionResult> GetRating(string userId, string productId)
 		{
 			try
 			{
 				var rating = await _unitOfWork.Ratings.GetByAsync(r => r.UserId == userId && r.ProductId == productId);
-
+				
+				if (rating == null) {
+					_logger.LogWarning($"Failed GET attempt in {nameof(GetRating)}. Could not find a rating with userId={userId} and productId={productId}.");
+					
+					return NotFound($"Could not find a rating with userId={userId} and productId={productId}");
+				}
+				
 				return Ok(_mapper.Map<RatingDto>(rating));
 			}
 			catch (Exception ex)
 			{
 				_logger.LogError(ex, $"Something went wrong when trying to access the data of the rating with userId={userId} and productId={productId}.");
+				
 				return StatusCode(500, "Internal Server Error.");
 			}
 		}
-        
-        [HttpGet]
-		[Route("ratings/seed")]
-		public async Task<IActionResult> SeedDatabase()
-		{
-			List<RatingDto> ratings = RatingSeeder.Seed(null);
-			await _unitOfWork.Ratings.InsertRangeAsync(_mapper.Map<List<Rating>>(ratings));
+		
+		[HttpPost]
+		[ProducesResponseType(StatusCodes.Status201Created)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+		public async Task<IActionResult> CreateRating([FromBody] RatingCreateDto ratingDto) {
+			if (!ModelState.IsValid) {
+				_logger.LogInformation($"Failed POST attempt in {nameof(CreateRating)}. The request body is not valid.");
+				
+				return BadRequest(ModelState);
+			}
 			
-			// string output = "";
-			// foreach (var rating in ratings)
-			// {
-			// 	output += rating.RatingValue.ToString() + ", ";
-			// }
-
-			await _unitOfWork.Save();
-			return Ok("Data added successfully."); // + output );
+			try {
+				var rating = _mapper.Map<Rating>(ratingDto);
+				
+				var exists = await _unitOfWork.Ratings.existsAsync(r => r.ProductId == rating.ProductId && r.UserId == rating.UserId);
+				
+				if (exists) {
+					_logger.LogInformation($"Invalid POST attempt in {nameof(CreateRating)}. The current user has already rated the same product before.");
+					
+					return BadRequest("The current user has already rated the same product before.");
+				}
+				
+				await _unitOfWork.Ratings.InsertAsync(rating);
+				await _unitOfWork.Save();
+				
+				return CreatedAtRoute("GetRating", new { Id = rating.ProductId }, rating);
+			}
+			catch (Exception ex) {
+				_logger.LogError(ex, $"Something went wrong when trying to create a new rating in the {nameof(CreateRating)}.");
+				
+				return StatusCode(500, "Internal Server Error.");
+			}
+		}
+		
+		[HttpPut("{userId}/{productId}")]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+		public async Task<IActionResult> UpdateRating(string userId, string productId, [FromBody] RatingUpdateDto ratingDto) {
+			if (!ModelState.IsValid) {
+				_logger.LogInformation($"Invalid PUT attempt in {nameof(UpdateRating)}. The request body is not valid.");
+				
+				return BadRequest(ModelState);
+			}
+			
+			if (userId != ratingDto.UserId || productId != ratingDto.ProductId) {
+				_logger.LogInformation($"Invalid PUT attempt in {nameof(UpdateRating)}. The user id and product id in the request body do not match the ones in the request url.");
+				
+				return BadRequest("The user id and product id in the request body do not match the ones in the request url.");
+			}
+			
+			try {
+				var rating = _mapper.Map<Rating>(ratingDto);
+				
+				var exists = await _unitOfWork.Ratings.existsAsync(r => r.ProductId == productId && r.UserId == userId);
+				
+				if (!exists) {
+					_logger.LogInformation($"Invalid PUT attempt in {nameof(UpdateRating)}. The user with id={userId} has not rated the product with product id={productId} before.");
+					
+					return NotFound($"The user with id={userId} has not rated the product with product id={productId} before.");
+				}
+				
+				rating.Timestamp = DateTime.Now;
+				
+				_unitOfWork.Ratings.Update(rating);
+				
+				await _unitOfWork.Save();
+				
+				return NoContent();
+			}
+			catch (Exception ex) {
+				_logger.LogError(ex, $"Something went wrong when trying to update a rating in the {nameof(UpdateRating)}.");
+				
+				return StatusCode(500, "Internal Server Error.");
+			}
+		}
+		
+		[HttpDelete("{userId}/{productId}")]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+		public async Task<IActionResult> DeleteRating(string userId, string productId) {
+			if (userId == null || productId == null) {
+				_logger.LogInformation($"Invalid DELETE attempt in {nameof(DeleteRating)}. Either the user id or product id is null.");
+				
+				return BadRequest(ModelState);
+			}
+			
+			try {
+				var exists = await _unitOfWork.Ratings.existsAsync(r => r.UserId == userId && r.ProductId == productId);
+				
+				if (!exists) {
+					_logger.LogInformation($"Invalid DELETE attempt in {nameof(DeleteRating)}. The user with id={userId} has not rated the product with product id={productId} yet.");
+					
+					return NotFound($"The user with id={userId} has not rated the product with product id={productId} yet.");
+				}
+				
+				await _unitOfWork.Ratings.DeleteAsync(r => r.UserId == userId && r.ProductId == productId);
+				await _unitOfWork.Save();
+				
+				return NoContent();
+			}
+			catch (Exception ex) {
+				_logger.LogError(ex, $"Something went wrong when trying to delete a rating in the {nameof(DeleteRating)}.");
+				
+				return StatusCode(500, "Internal Server Error.");
+			}
 		}
     }
 }
