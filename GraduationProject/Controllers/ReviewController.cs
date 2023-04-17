2f3b2afb3b1f5788d9ef71a8df452aff433fc904
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-17 13:41:10 +0200

    Adding Get pages, Post, Put, and Delete endpoints.

--- GraduationProject/Controllers/ReviewController.cs
+++ GraduationProject/Controllers/ReviewController.cs
@@ -2,7 +2,6 @@ using AutoMapper;
 using GraduationProject.IRepository;
 using GraduationProject.Models;
 using GraduationProject.Models.Dto;
-using GraduationProject.Utils;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -28,72 +27,148 @@ namespace GraduationProject.Controllers
         [HttpGet]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        [Route("reviews/getAllReviews")]
-		public async Task<IActionResult> GetAllReviews()
-		{
-			try
-			{
-				var reviews = await _unitOfWork.Reviews.GetAllAsync();
+		public async Task<IActionResult> GetReviews([FromQuery] PagingFilter pagingFilter) {
+			try {
+				var reviews = await _unitOfWork.Reviews.GetPagedList(pagingFilter);
 
 				return Ok(_mapper.Map<IList<ReviewDto>>(reviews));
 			}
-			catch (Exception ex)
-			{
+			catch (Exception ex) {
 				_logger.LogError(ex, $"Something went wrong when trying to access all the review data.");
 				return StatusCode(500, "Internal Server Error.");
 			}
 		}
-        
-        [HttpGet]
+		
+		
+        [HttpGet("{userId}/{productId}")]
+		[ActionName(nameof(GetReview))]
 		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
-		[Route("reviews/user/{userId}/product/{productId}")]
-		public async Task<IActionResult> GetReview(string userId, string productId)
-		{
-			try
-			{
+		public async Task<IActionResult> GetReview(string userId, string productId) {
+			try {
 				var review = await _unitOfWork.Reviews.GetByAsync(r => r.UserId == userId && r.ProductId == productId);
-
+				
+				if (review == null) {
+					_logger.LogWarning($"Failed GET attempt in {nameof(GetReview)}. Could not find a review with userId={userId} and productId={productId}.");
+					
+					return NotFound($"Could not find a review with userId={userId} and productId={productId}");
+				}
+				
 				return Ok(_mapper.Map<ReviewDto>(review));
 			}
-			catch (Exception ex)
-			{
+			catch (Exception ex) {
 				_logger.LogError(ex, $"Something went wrong when trying to access the data of the review with userId={userId} and productId={productId}.");
 				return StatusCode(500, "Internal Server Error.");
 			}
 		}
-        
-        [HttpGet]
-		[Route("reviews/seedClothings")]
-		public async Task<IActionResult> SeedClothingDatabase()
-		{
-			List<ReviewDto> reviews = ReviewSeeder.Seed(0);
-			await _unitOfWork.Reviews.InsertRangeAsync(_mapper.Map<List<Review>>(reviews));
-			//string output = "";
-			//foreach (var review in reviews)
-			//{
-			//	output += review.ReviewText.ToString() + ", ";
-			//}
-
-			await _unitOfWork.Save();
-			return Ok("Data added successfully."); //  + output 
-		}
-        
-        [HttpGet]
-		[Route("reviews/seedHome_Kitchen")]
-		public async Task<IActionResult> SeedHome_KitchenDatabase()
-		{
-			List<ReviewDto> reviews = ReviewSeeder.Seed(1);
-			await _unitOfWork.Reviews.InsertRangeAsync(_mapper.Map<List<Review>>(reviews));
+		
+		[HttpPost]
+		[ProducesResponseType(StatusCodes.Status201Created)]
+		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+		public async Task<IActionResult> CreateReview([FromBody] ReviewDto reviewDto) {
+			if (!ModelState.IsValid) {
+				_logger.LogInformation($"Invalid POST attempt in {nameof(CreateReview)}. The request body is not valid.");
+				
+				return BadRequest(ModelState);
+			}
 			
-            // string output = "";
-			// foreach (var review in reviews)
-			// {
-			// 	output += review.ReviewText.ToString() + ", ";
-			// }
-
-			await _unitOfWork.Save();
-			return Ok("Data added successfully."); // " + output );
+			try {
+				var review = _mapper.Map<Review>(reviewDto);
+				
+				var exists = await _unitOfWork.Reviews.existsAsync(r => r.Id == review.Id);
+				
+				if (exists) {
+					_logger.LogInformation($"Invalid POST attempt in {nameof(CreateReview)}. A review with same key already exists.");
+					
+					return BadRequest("A review with same key already exists.");
+				}
+				
+				await _unitOfWork.Reviews.InsertAsync(review);
+				await _unitOfWork.Save();
+				
+				return CreatedAtRoute("GetReview", new { Id = review.Id }, review);
+			}
+			catch (Exception ex) {
+				_logger.LogError(ex, $"Something went wrong when trying to create a new review in the {nameof(CreateReview)}.");
+				
+				return StatusCode(500, "Internal Server Error.");
+			}
+		}
+		
+		[HttpPut("{userId}/{productId}")]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+		public async Task<IActionResult> UpdateReview(string userId, string productId, [FromBody] ReviewUpdateDto reviewDto) {
+			if (!ModelState.IsValid) {
+				_logger.LogInformation($"Invalid PUT attempt in {nameof(UpdateReview)}. The request body is not valid.");
+				
+				return BadRequest(ModelState);
+			}
+			
+			if (userId != reviewDto.UserId || productId != reviewDto.ProductId) {
+				_logger.LogInformation($"Invalid PUT attempt in {nameof(UpdateReview)}. The user id and product id in the request body do not match the ones in the request url.");
+				
+				return BadRequest($"The user id and product id in the request body do not match the ones in the request url.");
+			}
+			
+			try {
+				var review = _mapper.Map<Review>(reviewDto);
+				
+				var exists = await _unitOfWork.Ratings.existsAsync(r => r.ProductId == productId && r.UserId == userId);
+				
+				if (!exists) {
+					_logger.LogInformation($"Invalid PUT attempt in {nameof(UpdateReview)}. The user with id={userId} has not reviewed the product with product id={productId} before.");
+					
+					return BadRequest($"The user with id={userId} has not reviewed the product with product id={productId} before.");
+				}
+				
+				review.Timestamp = DateTime.Now;
+				
+				_unitOfWork.Reviews.Update(review);
+				await _unitOfWork.Save();
+				
+				return NoContent();
+			}
+			catch (Exception ex) {
+				_logger.LogError(ex, $"Something went wrong when trying to update a review in the {nameof(UpdateReview)}.");
+				
+				return StatusCode(500, "Internal Server Error.");
+			}
+		}
+		
+		[HttpDelete("{userId}/{productId}")]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+		public async Task<IActionResult> DeleteReview(string userId, string productId) {
+			if (userId == null || productId == null) {
+				_logger.LogInformation($"Invalid DELETE attempt in {nameof(DeleteReview)}. Either the user id or the product id is null.");
+				
+				return BadRequest(ModelState);
+			}
+			
+			try {
+				var exists = await _unitOfWork.Reviews.existsAsync(r => r.UserId == userId && r.ProductId == productId);
+				
+				if (!exists) {
+					_logger.LogInformation($"Invalid DELETE attempt in {nameof(DeleteReview)}. No reviews exist for the product ({productId}) and user ({userId}).");
+					
+					return NotFound($"No reviews exist for the product ({productId}) and user ({userId}).");
+				}
+				
+				await _unitOfWork.Reviews.DeleteAsync(r => r.UserId == userId && r.ProductId == productId);
+				await _unitOfWork.Save();
+				
+				return NoContent();
+			}
+			catch (Exception ex) {
+				_logger.LogError(ex, $"Something went wrong when trying to delete a review in the {nameof(DeleteReview)}.");
+				
+				return StatusCode(500, "Internal Server Error.");
+			}
 		}
     }
 }
