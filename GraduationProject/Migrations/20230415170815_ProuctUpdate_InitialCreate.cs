17615919d185f7b19a09c3e1400b044827b20b4a
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-17 14:02:07 +0200

    Update and rename 20230408223741_UpdateProduct-InitialCreate.cs to 20230415170815_ProuctUpdate_InitialCreate.cs

--- GraduationProject/Migrations/20230415170815_ProuctUpdate_InitialCreate.cs
+++ GraduationProject/Migrations/20230415170815_ProuctUpdate_InitialCreate.cs
@@ -1,47 +1,47 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace GraduationProject.Migrations
 {
-    public partial class UpdateProductInitialCreate : Migration
+    public partial class ProuctUpdate_InitialCreate : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "ProdutUpdates",
+                name: "ProductUpdates",
                 columns: table => new
                 {
                     CurrentProductId = table.Column<string>(type: "nvarchar(450)", nullable: false),
-                    ProductUpdateId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                    UpdatedProductId = table.Column<string>(type: "nvarchar(450)", nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_ProdutUpdates", x => new { x.CurrentProductId, x.ProductUpdateId });
+                    table.PrimaryKey("PK_ProductUpdates", x => new { x.CurrentProductId, x.UpdatedProductId });
                     table.ForeignKey(
-                        name: "FK_ProdutUpdates_Products_CurrentProductId",
+                        name: "FK_ProductUpdates_Products_CurrentProductId",
                         column: x => x.CurrentProductId,
                         principalTable: "Products",
                         principalColumn: "Id");
                     table.ForeignKey(
-                        name: "FK_ProdutUpdates_Products_ProductUpdateId",
-                        column: x => x.ProductUpdateId,
+                        name: "FK_ProductUpdates_Products_UpdatedProductId",
+                        column: x => x.UpdatedProductId,
                         principalTable: "Products",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
                 });
 
             migrationBuilder.CreateIndex(
-                name: "IX_ProdutUpdates_ProductUpdateId",
-                table: "ProdutUpdates",
-                column: "ProductUpdateId",
+                name: "IX_ProductUpdates_UpdatedProductId",
+                table: "ProductUpdates",
+                column: "UpdatedProductId",
                 unique: true);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "ProdutUpdates");
+                name: "ProductUpdates");
         }
     }
 }
