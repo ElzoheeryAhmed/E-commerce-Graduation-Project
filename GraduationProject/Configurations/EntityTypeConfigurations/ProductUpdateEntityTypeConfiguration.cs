d208df649f0a7a2e3cc7b812c0a65abcc83882be
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-17 13:30:19 +0200

    Update and rename UpdateProductEntityTypeConfiguration.cs to ProductUpdateEntityTypeConfiguration.cs

--- GraduationProject/Configurations/EntityTypeConfigurations/ProductUpdateEntityTypeConfiguration.cs
+++ GraduationProject/Configurations/EntityTypeConfigurations/ProductUpdateEntityTypeConfiguration.cs
@@ -1,27 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore;
 using GraduationProject.Models;
 
 namespace GraduationProject.Configurations.EntityTypeConfigurations
 {
-    class UpdateProductEntityTypeConfiguration : IEntityTypeConfiguration<UpdateProduct>
+    class ProductUpdateEntityTypeConfiguration : IEntityTypeConfiguration<ProductUpdate>
     {
-        public void Configure(EntityTypeBuilder<UpdateProduct> builder)
+        public void Configure(EntityTypeBuilder<ProductUpdate> builder)
         {
             //Relationship
             builder.HasOne(b => b.CurrentProduct)
                 .WithMany()
                 .HasForeignKey(d => d.CurrentProductId)
                 .OnDelete(DeleteBehavior.NoAction);///////Be careful ya 3am
-            builder.HasOne(b => b.ProductUpdate)
+            builder.HasOne(b => b.UpdatedProduct)
                 .WithOne()
-                .HasForeignKey<UpdateProduct>(b => b.ProductUpdateId)
+                .HasForeignKey<ProductUpdate>(b => b.UpdatedProductId)
                 .OnDelete(DeleteBehavior.Cascade);
 
 
 
             //key
-            builder.HasKey(b => new { b.CurrentProductId, b.ProductUpdateId });
+            builder.HasKey(b => new { b.CurrentProductId, b.UpdatedProductId });
         }
     }
 }
