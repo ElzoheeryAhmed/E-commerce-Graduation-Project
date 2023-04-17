5bdb849cc092b0511c4401f999dafe2d74f87861
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-17 14:14:56 +0200

    Update RatingDto.cs

--- GraduationProject/Models/Dto/RatingDto.cs
+++ GraduationProject/Models/Dto/RatingDto.cs
@@ -6,14 +6,28 @@ namespace GraduationProject.Models.Dto
 {
     public class RatingCreateDto
     {
+        [Required]
         public string UserId { get; set; }
-        
+        [Required]
         public string ProductId { get; set; }
+        
+        [Required]
+        [Range(0, 5, ErrorMessage = "Rating value must be between 0 and 5.")]
         public double RatingValue { get; set; }
     }
     
     public class RatingDto : RatingCreateDto
     {
         public DateTime Timestamp { get; set; }
+    }
+    
+    public class RatingUpdateDto
+    {
+        public string UserId { get; set; }
+        public string ProductId { get; set; }
+        
+        [Required]
+        [Range(0, 5, ErrorMessage = "Rating value must be between 0 and 5.")]
+        public double RatingValue { get; set; }
     }
 }
