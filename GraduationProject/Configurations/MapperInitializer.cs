8547cb01540260c3b17e8c7aabb1172aafcf0640
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-21 23:50:05 +0200

    Conditionally map ProductCategory if not null.

--- GraduationProject/Configurations/MapperInitializer.cs
+++ GraduationProject/Configurations/MapperInitializer.cs
@@ -4,17 +4,29 @@ using GraduationProject.Models.Dto;
 
 namespace GraduationProject.Configurations
 {
-	public class MapperInitializer : Profile
-	{
-		public MapperInitializer()
-		{
-			CreateMap<Product, ProductCreateDto>()
-				.ReverseMap()
+	public class MapperInitializer : Profile {
+		public MapperInitializer() {
+			CreateMap<ProductCreateDto, Product>()
+				.ForMember(dest => dest.ProductCategories, opt => opt.MapFrom(src => src.ProductCategories.Select(pc => new ProductCategoryJoin { ProductCategoryId = pc })))
 				.ForAllMembers(opt => opt.Condition((src, dest, sourceMember) => sourceMember != null));
 			
 			CreateMap<Product, ProductDto>()
-				.ReverseMap()
+				.ForMember(dest => dest.ProductCategories, opts => {
+					opts.Condition(src => (src.ProductCategories != null) && (src.ProductCategories.Count > 0) && (src.ProductCategories[0] != null));
+					opts.MapFrom(src => src.ProductCategories.Select(pc => src.ProductCategories[0].ProductCategory != null ?
+						new ProductCategoryDto { Id = pc.ProductCategoryId, Name = pc.ProductCategory.Name } : new ProductCategoryDto { Id = pc.ProductCategoryId } ));
+				}).ForAllMembers(opt => opt.Condition((src, dest, sourceMember) => sourceMember != null));
+			
+			CreateMap<ProductDto, Product>()
+				.ForMember(dest => dest.ProductCategories, opt => opt.MapFrom(src => src.ProductCategories.Select(pc => new ProductCategoryJoin { ProductCategoryId = pc.Id })))
 				.ForAllMembers(opt => opt.Condition((src, dest, sourceMember) => sourceMember != null));
+			
+			CreateMap<Product, ProductDtoWithBrand>()
+				.ForMember(dest => dest.ProductCategories, opts => {
+					opts.Condition(src => (src.ProductCategories != null) && (src.ProductCategories.Count > 0) && (src.ProductCategories[0] != null));
+					opts.MapFrom(src => src.ProductCategories.Select(pc => src.ProductCategories[0].ProductCategory != null ?
+						new ProductCategoryDto { Id = pc.ProductCategoryId, Name = pc.ProductCategory.Name } : new ProductCategoryDto { Id = pc.ProductCategoryId } ));
+				}).ForAllMembers(opt => opt.Condition((src, dest, sourceMember) => sourceMember != null));
 			
 			CreateMap<ProductCategory, ProductCategoryCreateDto>()
 				.ReverseMap()
@@ -25,9 +37,19 @@ namespace GraduationProject.Configurations
 				.ForAllMembers(opt => opt.Condition((src, dest, sourceMember) => sourceMember != null));
 			
 			CreateMap<Product, ProductUpdateDto>()
-				.ReverseMap()
+				.ForMember(dest => dest.ProductCategories, opt => opt.MapFrom(src => src.ProductCategories.Select(pc => pc.ProductCategoryId)))
 				.ForAllMembers(opt => opt.Condition((src, dest, sourceMember) => sourceMember != null));
 			
+			CreateMap<ProductUpdateDto, Product>()
+				.ForMember(dest => dest.ProductCategories, opts => {
+					opts.Condition(src => (src.ProductCategories != null) && (src.ProductCategories.Count > 0));
+					opts.MapFrom(src => src.ProductCategories.Select(pc => new ProductCategoryJoin { ProductCategoryId = pc }));})
+				.ForAllMembers(opt => opt.Condition((src, dest, sourceMember) => sourceMember != null));
+			
+			
+			CreateMap<Brand, BrandDto>()
+				.ReverseMap()
+				.ForAllMembers(opt => opt.Condition((src, dest, sourceMember) => sourceMember != null));
 			
 			CreateMap<User, UserLoginDto>()
 				.ReverseMap()
