f126b466e2c6e6a0c1f707f7d7e318ee946e0635
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-22 00:33:26 +0200

    Update ProductCategoryDto.cs

--- GraduationProject/Models/Dto/ProductCategoryDto.cs
+++ GraduationProject/Models/Dto/ProductCategoryDto.cs
@@ -1,17 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GraduationProject.Models.Dto
 {
-    public class ProductCategoryCreateDto
-    {
+    public class ProductCategoryCreateDto {
         public string Name { get; set; }
     }
     
-    public class ProductCategoryDto : ProductCategoryCreateDto
-    {
+    public class ProductCategoryDto : ProductCategoryCreateDto {
+        public int Id { get; set; }
+    }
+    
+    public class ProductCategoryGetDto {
         public int Id { get; set; }
     }
 }
