aa26134f80e31a6633a77f91ba38e5cd0f0a8945
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-21 23:44:04 +0200

    Add files via upload

---
+++ GraduationProject/Models/Interfaces/IProductInfo.cs
@@ -0,0 +1,14 @@
+
+namespace GraduationProject.Models.Interfaces
+{
+    public interface IProductInfo {
+        decimal Price { get; set; }
+        decimal Discount { get; set; }
+        int BrandId { get; set; }
+    }
+    
+    public interface IProductInfoWithVotes : IProductInfo {
+        double VoteAverage { get; set; }
+        int VoteCount { get; set; }
+    }
+}
