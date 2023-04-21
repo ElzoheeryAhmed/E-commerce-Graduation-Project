d90eabfa0b338628857e1b2e3dfc1cb20a96ccfb
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-22 00:34:24 +0200

    Update ProductDto.cs

--- GraduationProject/Models/Dto/ProductDto.cs
+++ GraduationProject/Models/Dto/ProductDto.cs
@@ -1,10 +1,10 @@
+using GraduationProject.Models.Interfaces;
 using GraduationProject.Models.ModelEnums;
 using System.ComponentModel.DataAnnotations;
 
 namespace GraduationProject.Models.Dto
 {
-	public class ProductCreateDto
-	{
+	public class ProductCreateDto : IProductInfo {
 		[Required]
 		public string Title { get; set; }
 		
@@ -17,47 +17,45 @@ namespace GraduationProject.Models.Dto
 		
 		[Range(0.0, 100_000_0000.0, ErrorMessage = "Discount must be greater than or equal to 0. Max discount allowed is $1 Million.")]
 		public decimal Discount { get; set; }
-		
-		public int BrandId { get; set; }
-		
 		// public string MainCategory { get; set; }
-		public List<ProductCategoryCreateDto> ProductCategories { get; set; } = new List<ProductCategoryCreateDto>();
 		public string Features { get; set; }
 		public string HighResImageURLs { get; set; }
+		public int Quantity { get; set; }
+		
+		public int BrandId { get; set; }
+		public List<int> ProductCategories { get; set; } = new List<int>();
 	}
 	
-	public class ProductDto : ProductCreateDto
-	{
-		
+	public class ProductDto : ProductCreateDto, IProductInfoWithVotes {
 		[Required]
 		public string Id { get; set; }
-		public ProductStatus Status { get; set; }
 		public int VoteCount { get; set; }
 		public DateTime DateAdded { get; set; }
 		public double VoteAverage { get; set; }
 		public List<ProductCategoryDto> ProductCategories { get; set; } = new List<ProductCategoryDto>();
 	}
 	
+	public class ProductDtoWithBrand : ProductDto, IProductInfoWithVotes {
+		// public ProductStatus Status { get; set; }
+		public BrandDto Brand { get; set; }
+	}
 	
-	public class ProductUpdateDto
-	{
+	public class ProductUpdateDto : IProductInfo {
 		public string? Title { get; set; }
 		public string? Description { get; set; }
 		
-		[Range(0.01, 100_000_0000.0, ErrorMessage = "Price must be greater than 0. Max price allowed is $1 Million.")]
+		[Range(0.00, 100_000_0000.0, ErrorMessage = "Price must be greater than 0. Max price allowed is $1 Million.")]
 		public decimal Price { get; set; }
 		
 		[Range(0.0, 100_000_0000.0, ErrorMessage = "Discount must be greater than or equal to 0. Max discount allowed is $1 Million.")]
 		public decimal Discount { get; set; }
-		
-		public Brand Brand { get; set; }
-		
+
 		// public string MainCategory { get; set; }
-		public ICollection<ProductCategoryDto>? ProductCategories { get; set; } = new HashSet<ProductCategoryDto>();
 		public string? Features { get; set; }
 		public string? HighResImageURLs { get; set; }
-		public ProductStatus Status { get; set; }
-		public int VoteCount { get; set; }
-		public double VoteAverage { get; set; }
+		// public int VoteCount { get; set; }
+		// public double VoteAverage { get; set; }
+		public int BrandId { get; set; }
+		public List<int>? ProductCategories { get; set; } = new List<int>();
 	}
 }
