a6d3be35099fa30ebd184d7e10149bec309cba62
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-22 00:41:02 +0200

    Adding support for ordering and field- & record-based filtering.

--- GraduationProject/Repository/GenericRepository.cs
+++ GraduationProject/Repository/GenericRepository.cs
@@ -8,87 +8,144 @@ using X.PagedList;
 
 namespace GraduationProject.Repository
 {
-	public class GenericRepository<T> : IGenericRepository<T> where T : class
-	{
+	public class GenericRepository<TSource> : IGenericRepository<TSource> where TSource : class {
 		private readonly AppDbContext _context;
-		private readonly DbSet<T> _db;
+		private readonly DbSet<TSource> _db;
 
 		public GenericRepository(AppDbContext context) {
 			_context = context;
-			_db = _context.Set<T>();
+			_db = _context.Set<TSource>();
 		}
 		
 		
-		public async Task<bool> existsAsync(Expression<Func<T, bool>> expression) {
+		public async Task<bool> existsAsync(Expression<Func<TSource, bool>> expression) {
 			return await _db.AnyAsync(expression);
 		}
 		
-		public async Task<T> GetByAsync(Expression<Func<T, bool>> expression = null, List<string> includes = null) {
-			// Create an IQueryable<T> object from the _db property, which represents the database table for type T.
-			IQueryable<T> query = _db;
-
-			if (includes != null) {
-				foreach (var includeProperty in includes) {
+		public async Task<TSource>? GetByAsync(Expression<Func<TSource, bool>> expression = null, bool asNoTracking = false, List<string>? includeEntities = null) {
+			// Create an IQueryable<TSource> object from the _db property, which represents the database table for type T.
+			IQueryable<TSource> query = _db;
+			
+			if (includeEntities != null) {
+				foreach (var entity in includeEntities) {
 					// If the includes parameter is not null, load the related entities using the Include method.
-					query = query.Include(includeProperty);
+					query = query.Include(entity);
 				}
 			}
-
+			
+			// Calling AsNoTracking() before calling FirstOrDefaultAsync() can improve performance by reducing the amount of work that Entity Framework needs to do to track changes to the entity. Since GetByAsync is only retrieving an entity and not modifying it, there is no need to track changes to the entity, hence AsNoTracking() is used.
+			if (asNoTracking) {
+				query = query.AsNoTracking();
+			}
+			
+			else {
+				query = query.AsTracking();
+			}
+			
 			// Execute the query and retrieve the first entity that matches the criteria using the FirstOrDefaultAsync method.
 			// If the expression parameter is not null, filter the query by the given expression.
-			// Calling AsNoTracking() before calling FirstOrDefaultAsync() can improve performance by reducing the amount of work that Entity Framework needs to do to track changes to the entity. Since GetByAsync is only retrieving an entity and not modifying it, there is no need to track changes to the entity, hence AsNoTracking() is used.
-			return await query.AsNoTracking().FirstOrDefaultAsync(expression);
+			return await query.FirstOrDefaultAsync(expression);
 		}
 
-		public async Task<IList<T>> GetAllAsync(Expression<Func<T, bool>> expression = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string> includes = null) {
-			IQueryable<T> query = _db;
+		public async Task<IEnumerable<TSource>>? GetAllAsync(List<Expression<Func<TSource, bool>>>? expressions = null, IPagingFilter? pagingfilter=null, bool asNoTracking = false, List<string>? includeEntities = null, string? orderBy = null) {
+			IQueryable<TSource> query = _db;
 
+			if (expressions != null) {
+				// https://stackoverflow.com/questions/4098343/entity-framework-where-method-chaining
+				foreach (var expression in expressions) {
+					query = query.Where(expression);
+				}
+			}
+			
+			if (includeEntities != null) {
+				foreach (var entity in includeEntities) {
+					query = query.Include(entity);
+				}
+			}
+			
+			// if (orderBy != null) {
+			// 	query = orderBy(query);
+			// }
+			
+			if (!string.IsNullOrWhiteSpace(orderBy)) {
+				query = query.OrderBy(orderBy);
+			}
+			
+			if (asNoTracking) {
+				query = query.AsNoTracking();
+			}
+			
+			else {
+				query = query.AsTracking();
+			}
+			
+			if (pagingfilter != null) {
+				return await query.ToPagedListAsync(pagingfilter.PageNumber, pagingfilter.PageSize);
+			}
+			
+			return await query.ToListAsync();
+		}
+
+		public async Task<TSource>? GetFieldByAsync(Expression<Func<TSource, bool>>? expression, Expression<Func<TSource, TSource>> fieldSelector, bool asNoTracking = false, string? includeEntity = null) {
+			IQueryable<TSource> query = _db;
+
+			if (includeEntity != null) {
+				query = query.Include(includeEntity);
+			}
+			
+			if (asNoTracking) {
+				query = query.AsNoTracking();
+			}
+			
+			else {
+				query = query.AsTracking();
+			}
+			
+			// Select the field that matches the selection criteria using the Select method.
+			return await query.Where(expression).Select(fieldSelector).FirstOrDefaultAsync();
+		}
+
+		public async Task<IEnumerable<TSource>>? GetFieldFromAllAsync(Expression<Func<TSource, TSource>> fieldSelector, Expression<Func<TSource, bool>>? expression = null, IPagingFilter? pagingfilter=null, bool asNoTracking = false, string includeEntity = null, string? orderBy = null) {
+			IQueryable<TSource> query = _db;
+			
 			if (expression != null) {
 				query = query.Where(expression);
 			}
-
-			if (includes != null) {
-				foreach (var includeProperty in includes) {
-					query = query.Include(includeProperty);
-				}
+			
+			if (includeEntity != null) {
+				query = query.Include(includeEntity);
 			}
-
-			if (orderBy != null) {
-				query = orderBy(query);
+			
+			query = query.Select(fieldSelector);
+			
+			if (!string.IsNullOrWhiteSpace(orderBy)) {
+				query = query.OrderBy(orderBy);
 			}
-
-			return await query.AsNoTracking().ToListAsync();
+			
+			if (asNoTracking) {
+				query = query.AsNoTracking();
+			}
+			
+			else {
+				query = query.AsTracking();
+			}
+			
+			if (pagingfilter != null) {
+				return await query.ToPagedListAsync(pagingfilter.PageNumber, pagingfilter.PageSize);
+			}
+			
+			return await query.ToListAsync();
 		}
 
-		public async Task<IPagedList<T>> GetPagedList(PagingFilter pagingfilter, Expression<Func<T, bool>> expression = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string> includes = null) {
-            IQueryable<T> query = _db;
-
-			if (expression != null) {
-				query = query.Where(expression);
-			}
-
-			if (includes != null) {
-				foreach (var includeProperty in includes) {
-					query = query.Include(includeProperty);
-				}
-			}
-
-			if (orderBy != null) {
-				query = orderBy(query);
-			}
-
-			return await query.AsNoTracking().ToPagedListAsync(pagingfilter.PageNumber, pagingfilter.PageSize);
-        }
-		
-		public async Task InsertAsync(T entity) {
+		public async Task InsertAsync(TSource entity) {
 			await _db.AddAsync(entity);
 		}
 
-		public async Task InsertRangeAsync(IEnumerable<T> entities) {
+		public async Task InsertRangeAsync(IEnumerable<TSource> entities) {
 			await _db.AddRangeAsync(entities);
 		}
 
-		public async Task<bool> DeleteAsync(Expression<Func<T, bool>> expression) {
+		public async Task<bool> DeleteAsync(Expression<Func<TSource, bool>> expression) {
 			var entity = await _db.AsNoTracking().FirstOrDefaultAsync(expression);
 
 			if (entity == null) {
@@ -100,11 +157,11 @@ namespace GraduationProject.Repository
 			return true;
 		}
 
-		public void DeleteRangeAsync(IEnumerable<T> entities) {
+		public void DeleteRangeAsync(IEnumerable<TSource> entities) {
 			_db.RemoveRange(entities);
 		}
 
-		public void Update(T entity) {
+		public void Update(TSource entity) {
 			// This line attaches the entity to the _db context. Attaching an entity to a context allows the context to track changes to the entity and detect when it should be updated in the database.
 			_db.Attach(entity);
 
