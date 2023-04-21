21bae0db1bc8ea04963e9aba68ceefe24505c1e3
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-22 00:32:44 +0200

    Update PagingFilter.cs

--- GraduationProject/Models/Dto/PagingFilter.cs
+++ GraduationProject/Models/Dto/PagingFilter.cs
@@ -1,18 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GraduationProject.Models.Dto
 {
-    public class PagingFilter
+    public interface IPagingFilter
     {
+        int PageNumber { get; set; }
+        int PageSize { get; set; }
+    }
+
+    public class PagingFilter : IPagingFilter {
         const int maxPageSize = 50;
         public int PageNumber { get; set; } = 1;
         private int _pageSize = 10;
         
-        public int PageSize
-        {
+        public int PageSize {
+            get => _pageSize;
+            set => _pageSize = (value > maxPageSize) ? maxPageSize : value;
+        }
+    }
+    
+    public class Categories_BrandsPagingFilter : IPagingFilter {
+        const int maxPageSize = 5000;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 50;
+        
+        public int PageSize {
             get => _pageSize;
             set => _pageSize = (value > maxPageSize) ? maxPageSize : value;
         }
