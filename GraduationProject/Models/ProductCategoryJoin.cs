ee08f539010223f7011365cbc1da303281cfd22a
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-22 00:26:35 +0200

    Update ProductCategoryJoin.cs

--- GraduationProject/Models/ProductCategoryJoin.cs
+++ GraduationProject/Models/ProductCategoryJoin.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GraduationProject.Models
 {
-    public class ProductCategoryJoin
-    {
+    public class ProductCategoryJoin {
         public string ProductId { get; set; }
         public int ProductCategoryId { get; set; }
-        // public Product Product { get; set; }
-        // public ProductCategory ProductCategory { get; set; }
+        public Product Product { get; set; }
+        public ProductCategory ProductCategory { get; set; }
     }
 }
