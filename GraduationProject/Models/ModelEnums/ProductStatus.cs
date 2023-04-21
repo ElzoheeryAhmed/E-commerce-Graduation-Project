1ba3be518a21a292e8c82f7ad26ff78b5434b0b5
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-22 00:31:49 +0200

    Starting the values from 1 to avoid int default value conflicts.

--- GraduationProject/Models/ModelEnums/ProductStatus.cs
+++ GraduationProject/Models/ModelEnums/ProductStatus.cs
@@ -2,7 +2,7 @@ namespace GraduationProject.Models.ModelEnums
 {
     public enum ProductStatus
     {
-        Updated, // Request_to_update
+        Updated=1, // Request_to_update
         Added,   // Request_to_add
         Current, // Approve
         Deleted // Request_to_delete
