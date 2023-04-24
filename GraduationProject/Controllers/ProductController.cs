b8e87de393fee3cb647fcef75a1ef56870374267
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-24 02:07:46 +0200

    Adding documentation and optimizing Db queries.

--- GraduationProject/Controllers/ProductController.cs
+++ GraduationProject/Controllers/ProductController.cs
@@ -1,11 +1,13 @@
 using System.Linq.Expressions;
 using AutoMapper;
+using GraduationProject.Controllers.FilterParameters;
 using GraduationProject.Controllers.Helpers;
 using GraduationProject.IRepository;
 using GraduationProject.Models;
 using GraduationProject.Models.Dto;
 using GraduationProject.Models.ModelEnums;
-using GraduationProject.Repository;
+using GraduationProject.Repository.Extensions;
+
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -25,42 +27,58 @@ namespace GraduationProject.Controllers
 			_mapper = mapper;
 		}
 		
+		/// <summary>
+		/// Get a paged list of products, with filtering and ordering options.
+		/// </summary>
+		/// <param name="pagingFilter">The page number and page size.</param>
+		/// <param name="fieldsFilters">A comma-separated list of fields to include and another one for fields to exclude from the results.</param>
+		/// <param name="recordFilters">A collection of product filters.</param>
+		/// <param name="orderBy">A comma-separated list of fields to order the results by. The results will be sorted in ascending order by default. To sort in descending order, prefix the field name with a hyphen (-).</param>
+		/// <remarks>Fields can be filtered and/or ordered by passing a comma-separated fields.</remarks>
+		/// <remarks>Does not return the product reviews and product ratings objects.</remarks>
+		/// <returns></returns>
+		/// <response code="200">Returns a paged list of products.</response>
+		/// <response code="400">If the 'OnlyIncludeFields' and 'FieldsToExclude' properties are both set.</response>
+		/// <response code="500">If an error occurs while trying to access the database.</response>
 		[HttpGet]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
 		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
-		public async Task<IActionResult> GetProducts([FromQuery] PagingFilter pagingFilter, [FromQuery] string? onlyIncludeFields = null, [FromQuery] string? fieldsToExclude = null, [FromQuery] int? filterByBrand = null, [FromQuery] string? orderBy = null) {
-			if (!string.IsNullOrWhiteSpace(onlyIncludeFields) && !string.IsNullOrWhiteSpace(fieldsToExclude)) {
-                return BadRequest("Either use 'onlyIncludeFields' or 'fieldsToExclude', not both.");
+		public async Task<IActionResult> GetProducts([FromQuery] PagingFilter pagingFilter, [FromQuery] ProductFieldsFilter fieldsFilters, [FromQuery] ProdcutRecordFilters recordFilters, [FromQuery] string? orderBy = null)
+		{
+			if (!string.IsNullOrWhiteSpace(fieldsFilters.OnlySelectFields) && !string.IsNullOrWhiteSpace(fieldsFilters.FieldsToExclude)) {
+                return BadRequest("Either use 'OnlyIncludeFields' or 'FieldsToExclude', not both.");
             }
 			
 			try {
-				List<string> entitiesToInclude = ProductHelper<Product>.GetNameOfEntitiesToInclude(onlyIncludeFields);
-				
+				// Removing the Reviews and Ratings entities from the list of entities to include, as they are not needed in this method and significantely increase the query time.
+				fieldsFilters.FieldsToExclude += ",Reviews,Ratings";
+				List<string> entitiesToInclude = ProductHelper<Product>.GetNameOfEntitiesToInclude(fieldsFilters);
 				
 				List<Expression<Func<Product, bool>>> filterExpression = new List<Expression<Func<Product, bool>>>() {p => p.Status == ProductStatus.Current};
+				filterExpression.AddRange(ProductHelper.GetProductFilters(recordFilters));
 				
-				if (filterByBrand != null && filterByBrand != 0) {
-					filterExpression.Add(p => p.Brand.Id == filterByBrand);
-				}
-				
-				
-				var products = await _unitOfWork.Products.GetAllAsync( filterExpression, pagingFilter, entitiesToInclude.Count == 0, entitiesToInclude, orderBy);
-				
-				if (fieldsToExclude != null) {
-					if (!fieldsToExclude.Contains("BrandId"))
-						fieldsToExclude += ",BrandId";
+				// We don't want to return the BrandId in the response as it is already returned in the Brand entity.
+				if (fieldsFilters.FieldsToExclude != null) {
+					if (!fieldsFilters.FieldsToExclude.Contains("BrandId"))
+						fieldsFilters.FieldsToExclude += ",BrandId";
 				}
 				
 				else {
-					fieldsToExclude = "BrandId";
+					fieldsFilters.FieldsToExclude = "BrandId";
 				}
 				
-				// Does not return {Brand, Reviews, Ratings}
+				// Dynamically create a select expression based on the fields to include and exclude instead of returning all the fields from the Db and filter them afterwards.
+				Expression<Func<Product, Product>> selectExpression = QueryableExtensions<Product>.EntityFieldsSelector(fieldsFilters);
+				
+				var products = await _unitOfWork.Products.GetAllAsync( filterExpression, pagingFilter, entitiesToInclude.Count == 0, entitiesToInclude, selectExpression, orderBy: orderBy);
+				
+				// Does not return {BrandId, Reviews, Ratings}
 				var json = JsonConvert.SerializeObject(_mapper.Map<IList<ProductDtoWithBrand>>(products), Formatting.Indented,
-					new GenericFieldBasedJsonConverter<ProductDtoWithBrand>(_fields, onlyIncludeFields, fieldsToExclude));
+					new GenericFieldBasedJsonConverter<ProductDtoWithBrand>(_fields, fieldsFilters));
 				
 				return Ok(json);
+				// return Ok(products);
 			}
 			
 			catch (Exception ex) {
@@ -69,22 +87,42 @@ namespace GraduationProject.Controllers
 			}
 		}
 
+		/// <summary>
+		/// Get a product by its id.
+		/// </summary>
+		/// <param name="id">The id of the product to get.</param>
+		/// <param name="fieldsFilters">A comma-separated list of fields to include and another one for fields to exclude from the results.</param>
+		/// <returns></returns>
+		/// <response code="200">Returns the product with the specified id.</response>
+		/// <response code="400">If the 'OnlyIncludeFields' and 'FieldsToExclude' properties are both set.</response>
+		/// <response code="404">If the product with the specified id is not found.</response>
 		[HttpGet("{id}")]
 		[ActionName(nameof(GetProduct))]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
 		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
-		public async Task<IActionResult> GetProduct(string id, [FromQuery] string? onlyIncludeFields = null, [FromQuery] string? fieldsToExclude = null) {
-			if (!string.IsNullOrWhiteSpace(onlyIncludeFields) && !string.IsNullOrWhiteSpace(fieldsToExclude)) {
-                return BadRequest("Either use 'onlyIncludeFields' or 'fieldsToExclude', not both.");
+		public async Task<IActionResult> GetProduct(string id, [FromQuery] ProductFieldsFilter fieldsFilters) {
+			if (!string.IsNullOrWhiteSpace(fieldsFilters.OnlySelectFields) && !string.IsNullOrWhiteSpace(fieldsFilters.FieldsToExclude)) {
+                return BadRequest("Either use 'OnlyIncludeFields' or 'FieldsToExclude', not both.");
             }
 			
 			try {
+				List<string> entitiesToInclude = ProductHelper<Product>.GetNameOfEntitiesToInclude(fieldsFilters);
 				
-				List<string> entitiesToInclude = ProductHelper<Product>.GetNameOfEntitiesToInclude(onlyIncludeFields);
+				if (fieldsFilters.FieldsToExclude != null) {
+					if (!fieldsFilters.FieldsToExclude.Contains("BrandId"))
+						fieldsFilters.FieldsToExclude += ",BrandId";
+				}
 				
-				var product = await _unitOfWork.Products.GetByAsync(p => p.Id == id, entitiesToInclude.Count == 0, entitiesToInclude);
+				else {
+					fieldsFilters.FieldsToExclude = "BrandId";
+				}
+				
+				// Dynamically create a select expression based on the fields to include and exclude instead of returning all the fields from the Db and filter them afterwards.
+				Expression<Func<Product, Product>> selectExpression = QueryableExtensions<Product>.EntityFieldsSelector(fieldsFilters);
+				
+				var product = await _unitOfWork.Products.GetByAsync(p => p.Id == id, entitiesToInclude.Count == 0, entitiesToInclude, selectExpression);
 				
 				
 				if (product == null) {
@@ -93,17 +131,8 @@ namespace GraduationProject.Controllers
 					return NotFound($"Could not find a product with id={id}.");
 				}
 				
-				if (fieldsToExclude != null) {
-					if (!fieldsToExclude.Contains("BrandId"))
-						fieldsToExclude += ",BrandId";
-				}
-				
-				else {
-					fieldsToExclude = "BrandId";
-				}
-				
 				var json = JsonConvert.SerializeObject(_mapper.Map<ProductDtoWithBrand>(product), Formatting.Indented,
-					new GenericFieldBasedJsonConverter<ProductDtoWithBrand>(_fields, onlyIncludeFields, fieldsToExclude));
+					new GenericFieldBasedJsonConverter<ProductDtoWithBrand>(_fields, fieldsFilters));
 				
 				return Ok(json);
 			}
@@ -112,7 +141,15 @@ namespace GraduationProject.Controllers
 				return StatusCode(500, $"Internal Server Error. Something went wrong when trying to access the data of the product with id={id}.");
 			}
 		}
-
+		
+		/// <summary>
+		/// Create a new product.
+		/// </summary>
+		/// <param name="productDto">The product to create.</param>
+		/// <returns></returns>
+		/// <response code="201">Returns the newly created product.</response>
+		/// <response code="400">If the product is null or if the given product model is invalid.</response>
+		/// <response code="500">If there was an error on the server while creating the product.</response>
 		[HttpPost]
 		[ProducesResponseType(StatusCodes.Status201Created)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -124,19 +161,19 @@ namespace GraduationProject.Controllers
 				return BadRequest(ModelState);
 			}
 			
-			if (productDto.ProductCategories.Contains(0)) {
+			if (productDto.ProductCategoriesIds.Contains(0)) {
 				_logger.LogInformation($"Invalid POST attempt in {nameof(CreateProduct)}. '0' cannot be used as a product category identifier.");
 				
 				return BadRequest("'0' cannot be used as a product category identifier.");
 			}
 			
-			if (productDto.ProductCategories.Count != productDto.ProductCategories.Distinct().Count()) {
+			if (productDto.ProductCategoriesIds.Count != productDto.ProductCategoriesIds.Distinct().Count()) {
 				_logger.LogInformation($"Invalid POST attempt in {nameof(CreateProduct)}. Duplicate product category identifiers are not allowed.");
 				
 				return BadRequest("Duplicate product category identifiers are not allowed.");
 			}
 			
-			if (productDto.ProductCategories.Count == 0) {
+			if (productDto.ProductCategoriesIds.Count == 0) {
 				_logger.LogInformation($"Invalid POST attempt in {nameof(CreateProduct)}. Product category identifiers cannot be left empty.");
 				
 				return BadRequest("Product category identifiers cannot be left empty.");
@@ -153,9 +190,11 @@ namespace GraduationProject.Controllers
 				
 				product.Status = ProductStatus.Added;
 				
-				await _unitOfWork.Products.InsertAsync(product);
+				await _unitOfWork.Products.InsertAsync(product);				
 				
 				await _unitOfWork.Save();
+				
+				// return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
 				
 				if (product == null) {
 					_logger.LogWarning($"Failed POST attempt in {nameof(CreateProduct)}. Could not create a new product.");
@@ -164,7 +203,7 @@ namespace GraduationProject.Controllers
 				}
 				
 				var json = JsonConvert.SerializeObject(_mapper.Map<ProductDto>(product), Formatting.Indented,
-					new GenericFieldBasedJsonConverter<ProductDto>(_fields, "", "VoteCount,VoteAverage,Ratings,Reviews"));
+					new GenericFieldBasedJsonConverter<ProductDto>(_fields, new ProductFieldsFilter() { FieldsToExclude = "VoteCount,VoteAverage,Ratings,Reviews" }));
 				
 				return Ok(json);
 			}
@@ -175,7 +214,18 @@ namespace GraduationProject.Controllers
 			}
 		}
 		
+		/// <summary>
+		/// Update an existing product.
+		/// </summary>
+		/// <param name="id">The id of the product to update.</param>
+		/// <param name="productDto">The product to update.</param>
+		/// <returns></returns>
+		/// <response code="200">Returns the updated product.</response>
+		/// <response code="400">If the product is null or if the given product model is invalid.</response>
+		/// <response code="404">If the product with the given id was not found.</response>
+		/// <response code="500">If there was an error on the server while updating the product.</response>
 		[HttpPut("{id}")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
 		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -186,16 +236,18 @@ namespace GraduationProject.Controllers
 				return BadRequest(ModelState);
 			}
 			
-			if (productDto.ProductCategories.Contains(0)) {
-				_logger.LogInformation($"Invalid POST attempt in {nameof(CreateProduct)}. '0' cannot be used as a product category identifier.");
+			if (productDto.ProductCategories != null) {
+				if ( productDto.ProductCategories.Contains(0)) {
+					_logger.LogInformation($"Invalid POST attempt in {nameof(CreateProduct)}. '0' cannot be used as a product category identifier.");
+					
+					return BadRequest("'0' cannot be used as a product category identifier.");
+				}
 				
-				return BadRequest("'0' cannot be used as a product category identifier.");
-			}
-			
-			if (productDto.ProductCategories.Count != productDto.ProductCategories.Distinct().Count()) {
-				_logger.LogInformation($"Invalid POST attempt in {nameof(CreateProduct)}. Duplicate product category identifiers are not allowed.");
-				
-				return BadRequest("Duplicate product category identifiers are not allowed.");
+				if (productDto.ProductCategories.Count != productDto.ProductCategories.Distinct().Count()) {
+					_logger.LogInformation($"Invalid POST attempt in {nameof(CreateProduct)}. Duplicate product category identifiers are not allowed.");
+					
+					return BadRequest("Duplicate product category identifiers are not allowed.");
+				}
 			}
 			
 			// Request body cannot be an empty object.
@@ -206,7 +258,11 @@ namespace GraduationProject.Controllers
 			}
 			
 			try {
-				var product = await _unitOfWork.Products.GetByAsync(p => p.Id == id, false, new List<string> { "ProductCategories.ProductCategory", "Brand" });
+				// Dynamically create a select expression based on the fields to include and exclude instead of returning all the fields from the Db and filter them afterwards.
+				ProductFieldsFilter fieldsFilters = new ProductFieldsFilter() { OnlySelectFields = "Id,Quantity,Title,Description,Price,Discount,Features,HighResImageURLs,BrandId,ProductCategories", FieldsToExclude = "VoteCount,VoteAverage,Ratings,Reviews,Status"};
+				Expression<Func<Product, Product>> selectExpression = QueryableExtensions<Product>.EntityFieldsSelector(fieldsFilters);
+				
+				var product = await _unitOfWork.Products.GetByAsync(p => p.Id == id, false, new List<string> { "ProductCategories.ProductCategory" }, selectExpression);
 				
 				// This is necessary to avoid the error "The property 'Product.Id' is part of a key and so cannot be modified or marked as modified. To change the principal of an existing entity with an identifying foreign key, first delete the dependent and invoke 'SaveChanges', and then associate the dependent with the new principal."
 				// In other words, to prevent updating the existing product, we need to detach it from the context.
@@ -227,23 +283,16 @@ namespace GraduationProject.Controllers
 				ProductHelper<ProductUpdateDto>.KeepOriginalIfNewIsZero(productDto, product);
 				
 				// If the productDto does not contain any product categories, keep the existing product categories.
-				if (productDto.ProductCategories.Count == 0)
+				if (productDto.ProductCategories == null || productDto.ProductCategories.Count == 0)
 					productDto.ProductCategories = product.ProductCategories.Select(pc => pc.ProductCategoryId).ToList();
 				
 				// Mapping the productDto to the product, overwriting existing fields. Any null fields in the productDto will be ignored when mapping.
 				_mapper.Map(productDto, product);
 				
-				// Updating the existing product. The BL requires that any update request is added as a new product.
-				// _unitOfWork.Products.Update(product);
-				// await _unitOfWork.Save();
-				// return NoContent();
-				
-				string currentproductId = product.Id;
-				
-				// This allows the database to generate a new Id.
+				// This allows the database to generate a new Id and preventing it from adding a new product with the same id.
 				product.Id = null;
 				
-				// This allows the database to populate the Brand fields itself.
+				// This allows the database to populate the Brand fields itself and preventing it from adding a new brand with the same brand id to the brands table.
 				product.Brand = null;
 				
 				// Creating a new product with the updated data and a new key.
@@ -251,11 +300,14 @@ namespace GraduationProject.Controllers
 				string newProductId = product.Id; // Get the generated Id of the new product.
 				
 				// Inserting the product update record.
-				await _unitOfWork.ProductUpdates.InsertAsync(new ProductUpdate {CurrentProductId = currentproductId, UpdatedProductId = newProductId});
+				await _unitOfWork.ProductUpdates.InsertAsync(new ProductUpdate {CurrentProductId = id, UpdatedProductId = newProductId});
 				
 				await _unitOfWork.Save();
 				
-				return CreatedAtAction(nameof(GetProduct), new { id = newProductId }, product);
+				var json = JsonConvert.SerializeObject(product, Formatting.Indented,
+					new GenericFieldBasedJsonConverter<Product>(fieldsFilters.OnlySelectFields.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToHashSet(), new ProductFieldsFilter()));
+				
+				return Ok(json);
 			}
 			catch (Exception ex) {
 				_logger.LogError(ex, $"Something went wrong when trying to update a product in the {nameof(UpdateProduct)}.");
@@ -264,7 +316,18 @@ namespace GraduationProject.Controllers
 			}
 		}
 		
+		
+		/// <summary>
+		/// Deletes a product.
+		/// </summary>
+		/// <param name="id">The id of the product to delete.</param>
+		/// <returns></returns>
+		/// <response code="200">If the product was successfully deleted.</response>
+		/// <response code="400">If the id is null or empty.</response>
+		/// <response code="404">If the product with the specified id doesn't exist.</response>
+		/// <response code="500">If there was an error on the server.</response>
 		[HttpDelete("{id}")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
 		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -285,7 +348,7 @@ namespace GraduationProject.Controllers
 				}
 				
 				product.Status = ProductStatus.Deleted;
-				
+
 				await _unitOfWork.Save();
 				
 				return Ok(product);
