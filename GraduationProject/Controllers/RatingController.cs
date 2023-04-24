c022c1b64fd5ca12a19374e476f6219c769b6e04
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-24 02:10:20 +0200

    Adding new APIs, documentation, and optimizing Db queries.

--- GraduationProject/Controllers/RatingController.cs
+++ GraduationProject/Controllers/RatingController.cs
@@ -1,7 +1,10 @@
+using System.Linq.Expressions;
 using AutoMapper;
+using GraduationProject.Controllers.FilterParameters;
 using GraduationProject.IRepository;
 using GraduationProject.Models;
 using GraduationProject.Models.Dto;
+using GraduationProject.Repository.Extensions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,57 +14,95 @@ namespace GraduationProject.Controllers
     [Route("api/[controller]")]
     public class RatingController : ControllerBase {
         private readonly IUnitOfWork _unitOfWork;
-        private readonly UserManager<User> _userManager;
 		private readonly ILogger<RatingController> _logger;
 		private readonly IMapper _mapper;
 
 		public RatingController(IUnitOfWork unitOfWork, UserManager<User> userManager, ILogger<RatingController> logger, IMapper mapper) {
 			_unitOfWork = unitOfWork;
-            _userManager = userManager;
 			_logger = logger;
 			_mapper = mapper;
 		}
         
-        [HttpGet]
+		/// <summary>
+		/// Get all ratings of a product.
+		/// </summary>
+		/// <param name="id">The id of the product.</param>
+		/// <param name="pagingFilter">The page number and page size.</param>
+		/// <param name="orderBy">A comma-separated list of fields to order the results by. The results will be sorted in ascending order by default. To sort in descending order, prefix the field name with a hyphen (-).</param>
+		/// <returns></returns>
+		/// <response code="200">Returns the ratings of the specified product.</response>
+		/// <response code="404">If the product does not exist.</response>
+		/// <response code="500">If something went wrong on the server.</response>
+        [HttpGet("{id}")]
 		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
-		public async Task<IActionResult> GerRatings([FromQuery] PagingFilter pagingFilter) {
+		public async Task<IActionResult> GetRatingsByProduct(string id, [FromQuery] PagingFilter pagingFilter, [FromQuery] string? orderBy = null) {
 			try {
-				var ratings = await _unitOfWork.Ratings.GetAllAsync(pagingFilter: pagingFilter);
-
-				return Ok(_mapper.Map<IList<RatingDto>>(ratings));
+				EntityFieldsFilter fieldsFilters = new EntityFieldsFilter() { OnlySelectFields = "UserId,RatingValue,Timestamp",  FieldsToExclude = "User,Product,ProductId" };
+				Expression<Func<Rating, Rating>> selectExpression = QueryableExtensions<Rating>.EntityFieldsSelector(fieldsFilters);
+				
+				var ratings = await _unitOfWork.Ratings.GetAllAsync(new List<Expression<Func<Rating, bool>>>() { r => r.ProductId == id }, pagingFilter, true, selectExpression: selectExpression, orderBy: orderBy);
+				
+				if (ratings == null) {
+					_logger.LogWarning($"Failed GET attempt in {nameof(GetRatingsByProduct)}. Could not find a product with id={id}.");
+					
+					return NotFound($"Could not find a product with id={id}.");
+				}
+				
+				return Ok(ratings);
 			}
 			catch (Exception ex) {
-				_logger.LogError(ex, $"Something went wrong when trying to access all the rating data.");
+				_logger.LogError(ex, $"Something went wrong when trying to access all the ratings of the specified product.");
 				return StatusCode(500, "Internal Server Error.");
 			}
 		}
         
+		/// <summary>
+		/// Get the rating of a product given by a user.
+		/// </summary>
+		/// <param name="userId">The id of the user.</param>
+		/// <param name="productId">The id of the product.</param>
+		/// <returns>Returns the rating of the product given by the user.</returns>
+		/// <response code="200">Returns the rating of the product given by the user.</response>
+		/// <response code="404">If the rating does not exist.</response>
+		/// <response code="500">If something went wrong on the server.</response>
         [HttpGet("{userId}/{productId}")]
-		[ActionName(nameof(GetRating))]
+		[ActionName(nameof(GetProductRatingByUser))]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
-		public async Task<IActionResult> GetRating(string userId, string productId) {
+		public async Task<IActionResult> GetProductRatingByUser(string userId, string productId) {
 			try {
-				var rating = await _unitOfWork.Ratings.GetByAsync(r => r.UserId == userId && r.ProductId == productId);
+				var rating = await _unitOfWork.Ratings.GetByAsync(
+					r => r.UserId == userId && r.ProductId == productId,
+					selectExpression: r => new Rating { RatingValue = r.RatingValue, Timestamp = r.Timestamp });
 				
 				if (rating == null) {
-					_logger.LogWarning($"Failed GET attempt in {nameof(GetRating)}. Could not find a rating with userId={userId} and productId={productId}.");
+					_logger.LogWarning($"Failed GET attempt in {nameof(GetProductRatingByUser)}. Could not find any ratings for userId={userId} and productId={productId}.");
 					
-					return NotFound($"Could not find a rating with userId={userId} and productId={productId}");
+					return NotFound($"Could not find any ratings with userId={userId} and productId={productId}");
 				}
 				
 				return Ok(_mapper.Map<RatingDto>(rating));
 			}
 			
 			catch (Exception ex) {
-				_logger.LogError(ex, $"Something went wrong when trying to access the data of the rating with userId={userId} and productId={productId}.");
+				_logger.LogError(ex, $"Something went wrong when trying to access the ratings for userId={userId} and productId={productId}.");
 				
 				return StatusCode(500, "Internal Server Error.");
 			}
 		}
 		
+		
+		/// <summary>
+		/// Create a new rating for a product.
+		/// </summary>
+		/// <param name="ratingDto">The rating to create.</param>
+		/// <returns>Returns the created rating.</returns>
+		/// <response code="201">Returns the created rating.</response>
+		/// <response code="400">If the request body is not valid or the current user has already rated the same product before.</response>
+		/// <response code="500">If something went wrong on the server.</response>
 		[HttpPost]
 		[ProducesResponseType(StatusCodes.Status201Created)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -76,7 +117,7 @@ namespace GraduationProject.Controllers
 			try {
 				var rating = _mapper.Map<Rating>(ratingDto);
 				
-				var exists = await _unitOfWork.Ratings.existsAsync(r => r.ProductId == rating.ProductId && r.UserId == rating.UserId);
+				var exists = await _unitOfWork.Ratings.ExistsAsync(r => r.ProductId == rating.ProductId && r.UserId == rating.UserId);
 				
 				if (exists) {
 					_logger.LogInformation($"Invalid POST attempt in {nameof(CreateRating)}. The current user has already rated the same product before.");
@@ -85,10 +126,18 @@ namespace GraduationProject.Controllers
 				}
 				
 				await _unitOfWork.Ratings.InsertAsync(rating);
+				
+				// Updating the VoteAverage and VoteCount properties of the product.
+				Product product = await _unitOfWork.Products.GetByAsync(p => p.Id == ratingDto.ProductId);
+				
+				product.VoteAverage = (product.VoteAverage*product.VoteCount + rating.RatingValue)/(product.VoteCount+1);
+				product.VoteCount++;
+				
 				await _unitOfWork.Save();
 				
-				return CreatedAtRoute("GetRating", new { Id = rating.ProductId }, rating);
+				return CreatedAtAction(nameof(GetProductRatingByUser), new { userId = rating.UserId, productId = rating.ProductId }, rating);
 			}
+			
 			catch (Exception ex) {
 				_logger.LogError(ex, $"Something went wrong when trying to create a new rating in the {nameof(CreateRating)}.");
 				
@@ -96,8 +145,19 @@ namespace GraduationProject.Controllers
 			}
 		}
 		
+		/// <summary>
+		/// Update a rating.
+		/// </summary>
+		/// <param name="userId">The id of the user.</param>
+		/// <param name="productId">The id of the product.</param>
+		/// <param name="ratingDto">The rating to update.</param>
+		/// <returns>Returns no content.</returns>
+		/// <response code="200">The updated rating.</response>
+		/// <response code="400">If the request body is not valid or the user id and product id in the request body do not match the ones in the request url.</response>
+		/// <response code="404">If the rating does not exist.</response>
+		/// <response code="500">If something went wrong on the server.</response>
 		[HttpPut("{userId}/{productId}")]
-		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
 		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -117,9 +177,12 @@ namespace GraduationProject.Controllers
 			try {
 				var rating = _mapper.Map<Rating>(ratingDto);
 				
-				var exists = await _unitOfWork.Ratings.existsAsync(r => r.ProductId == productId && r.UserId == userId);
+				EntityFieldsFilter fieldsFilters = new EntityFieldsFilter() { OnlySelectFields = "RatingValue,Timestamp",  FieldsToExclude = "User,Product,UserId,ProductId" };
+				Expression<Func<Rating, Rating>> selectExpression = QueryableExtensions<Rating>.EntityFieldsSelector(fieldsFilters);
 				
-				if (!exists) {
+				var existingRating = await _unitOfWork.Ratings.GetByAsync(r => r.ProductId == productId && r.UserId == userId, selectExpression: selectExpression);
+				
+				if (existingRating == null) {
 					_logger.LogInformation($"Invalid PUT attempt in {nameof(UpdateRating)}. The user with id={userId} has not rated the product with product id={productId} before.");
 					
 					return NotFound($"The user with id={userId} has not rated the product with product id={productId} before.");
@@ -127,11 +190,16 @@ namespace GraduationProject.Controllers
 				
 				rating.Timestamp = DateTime.Now;
 				
+				// Updating the VoteAverage and VoteCount properties of the product.
+				var product = await _unitOfWork.Products.GetByAsync(p => p.Id == productId);
+				
+				product.VoteAverage = (product.VoteAverage*product.VoteCount - existingRating.RatingValue + rating.RatingValue )/(product.VoteCount);
+				
 				_unitOfWork.Ratings.Update(rating);
 				
 				await _unitOfWork.Save();
 				
-				return NoContent();
+				return Ok(rating);
 			}
 			catch (Exception ex) {
 				_logger.LogError(ex, $"Something went wrong when trying to update a rating in the {nameof(UpdateRating)}.");
@@ -140,6 +208,16 @@ namespace GraduationProject.Controllers
 			}
 		}
 		
+		/// <summary>
+		/// Delete a rating.
+		/// </summary>
+		/// <param name="userId">The id of the user.</param>
+		/// <param name="productId">The id of the product.</param>
+		/// <returns>Returns no content.</returns>
+		/// <response code="204">The rating deleted successfully.</response>
+		/// <response code="400">If the user id or product id is null.</response>
+		/// <response code="404">If the rating does not exist.</response>
+		/// <response code="500">If something went wrong on the server.</response>
 		[HttpDelete("{userId}/{productId}")]
 		[ProducesResponseType(StatusCodes.Status204NoContent)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -153,15 +231,25 @@ namespace GraduationProject.Controllers
 			}
 			
 			try {
-				var exists = await _unitOfWork.Ratings.existsAsync(r => r.UserId == userId && r.ProductId == productId);
+				EntityFieldsFilter fieldsFilters = new EntityFieldsFilter() { OnlySelectFields = "RatingValue",  FieldsToExclude = "User,Product,UserId,ProductId,Timestamp" };
+				Expression<Func<Rating, Rating>> selectExpression = QueryableExtensions<Rating>.EntityFieldsSelector(fieldsFilters);
 				
-				if (!exists) {
+				var rating = await _unitOfWork.Ratings.GetByAsync(r => r.ProductId == productId && r.UserId == userId, selectExpression: selectExpression);
+				
+				if (rating == null) {
 					_logger.LogInformation($"Invalid DELETE attempt in {nameof(DeleteRating)}. The user with id={userId} has not rated the product with product id={productId} yet.");
 					
 					return NotFound($"The user with id={userId} has not rated the product with product id={productId} yet.");
 				}
 				
 				await _unitOfWork.Ratings.DeleteAsync(r => r.UserId == userId && r.ProductId == productId);
+				
+				// Updating the VoteAverage and VoteCount properties of the product.
+				Product product = await _unitOfWork.Products.GetByAsync(p => p.Id == productId);
+				
+				product.VoteAverage = (product.VoteAverage*product.VoteCount - rating.RatingValue)/(product.VoteCount - 1);
+				product.VoteCount--;
+				
 				await _unitOfWork.Save();
 				
 				return NoContent();
