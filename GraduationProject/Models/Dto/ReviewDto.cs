47691e2c703b46ec2513107877a74d9b7989ac96
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-04-24 02:30:38 +0200

    Update ReviewDto.cs

--- GraduationProject/Models/Dto/ReviewDto.cs
+++ GraduationProject/Models/Dto/ReviewDto.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GraduationProject.Models.Dto
 {
-    public class ReviewCreateDto
-    {
+    public class ReviewCreateDto {
         [Required]
         public string UserId { get; set; }
         
@@ -18,18 +13,12 @@ namespace GraduationProject.Models.Dto
         public string ReviewText { get; set; }
     }
     
-    public class ReviewDto : ReviewCreateDto
-    {
-        public string Id { get; set; }
+    public class ReviewDto : ReviewCreateDto {
+        public int Id { get; set; }
         public DateTime Timestamp { get; set; }
     }
     
-    public class ReviewUpdateDto
-    {
-        public string UserId { get; set; }
-        
-        public string ProductId { get; set; }
-        
+    public class ReviewUpdateDto {
         [Required]
         public string ReviewText { get; set; }
     }
