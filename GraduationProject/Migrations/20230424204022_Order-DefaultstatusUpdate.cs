79db5cdc778799235ec5c17dfc877e49612fe104
ElzoheeryAhmed <elzoheeryahmed1807813@gmail.com>
2023-04-25 08:25:35 +0000

    Adding new migration file for order entity update

---
+++ GraduationProject/Migrations/20230424204022_Order-DefaultstatusUpdate.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GraduationProject.Migrations
+{
+    public partial class OrderDefaultstatusUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Orders",
+                type: "varchar(15)",
+                nullable: false,
+                defaultValue: "Confirmed",
+                oldClrType: typeof(string),
+                oldType: "varchar(15)",
+                oldDefaultValue: "Ordered");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShippingAddress",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShippingAddress",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Orders",
+                type: "varchar(15)",
+                nullable: false,
+                defaultValue: "Ordered",
+                oldClrType: typeof(string),
+                oldType: "varchar(15)",
+                oldDefaultValue: "Confirmed");
+        }
+    }
+}
