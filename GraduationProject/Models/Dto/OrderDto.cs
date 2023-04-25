0592fa6244d54514deee80e66da9293709c7ad65
ElzoheeryAhmed <elzoheeryahmed1807813@gmail.com>
2023-04-25 08:28:55 +0000

    Adding order different dtos

---
+++ GraduationProject/Models/Dto/OrderDto.cs
@@ -0,0 +1,35 @@
+﻿namespace GraduationProject.Models.Dto
+{
+    public class OrderDto
+    {
+        
+        public string CustomerId { get; set; }
+        public string ShippingAddress { get; set; }
+
+        public ICollection<OrderItemDto> OrderItems { get; set; }
+        
+    }
+
+
+    public class OrderDetailsDto
+    {
+
+        public int Id { get; set; }
+        public String Status { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime? ReceiptDate { get; set; }
+        public string ShippingAddress { get; set; }
+
+        public ICollection<OrderItemDetailsDto> OrderItems { get; set; }
+        
+
+    }
+    public class OrderAdminDto:OrderDetailsDto
+    {
+
+        public string CustomerId { get; set; }
+
+
+    }
+
+}
