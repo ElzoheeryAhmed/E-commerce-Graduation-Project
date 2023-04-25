09b3ad0546f5eb4e45b0365da9b1e9c303002503
ElzoheeryAhmed <elzoheeryahmed1807813@gmail.com>
2023-04-25 14:56:35 +0000

    Adding cartItem Dtos

---
+++ GraduationProject/Models/Dto/CartItemDto.cs
@@ -0,0 +1,40 @@
+﻿namespace GraduationProject.Models.Dto
+{
+
+    public class CartItemDto
+    {
+
+        public string ProductId { get; set; }
+
+        public string CustomerId { get; set; }
+        public byte Quantity { get; set; }
+
+
+    }
+
+    public class CartItemkeyDto
+    {
+
+        public string ProductId { get; set; }
+
+        public string CustomerId { get; set; }
+
+
+    }
+
+    public class CartItemDetailsDto
+    {
+       
+            public string ProductId { get; set; }
+
+            public string Title { get; set; }
+
+            public decimal Price { get; set; }
+
+            public string HighResImageURLs { get; set; }
+
+            public byte Quantity { get; set; }
+
+        
+    }
+}
