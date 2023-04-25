e14255747dc5ca9c77c5aaf18b0ed83bfee8f543
ElzoheeryAhmed <elzoheeryahmed1807813@gmail.com>
2023-04-25 08:29:59 +0000

    Adding orderItem different dtos

---
+++ GraduationProject/Models/Dto/OrderItemDto.cs
@@ -0,0 +1,30 @@
+﻿namespace GraduationProject.Models.Dto
+{
+    public class OrderItemDto
+    {
+
+        public string ProductId { get; set; }
+
+        public byte Quantity { get; set; }
+
+
+    }
+
+    public class OrderItemDetailsDto
+    {
+
+        
+        public string ProductId { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string HighResImageURLs { get; set; }
+
+        public byte Quantity { get; set; }
+
+    }
+
+
+    }
