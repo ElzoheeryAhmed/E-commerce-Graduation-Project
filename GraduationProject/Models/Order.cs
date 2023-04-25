683afbcf3ee82559bdc8be4640be49d10a2eb9ef
ElzoheeryAhmed <elzoheeryahmed1807813@gmail.com>
2023-04-25 08:21:49 +0000

    update order statuses and add shipping address member

--- GraduationProject/Models/Order.cs
+++ GraduationProject/Models/Order.cs
@@ -3,17 +3,19 @@
 
         public enum OrderStatus : byte
         {
-            Ordered, Cancelled, InTheWay, Received
-
+            Confirmed,Shipped, Cancelled, Receipted,Returned
         }
         public class Order
         {
+        
             public int Id { get; set; }
             public OrderStatus Status { get; set; }
             public DateTime OrderDate { get; set; }
             public DateTime? ReceiptDate { get; set; }
+            public string ShippingAddress { get; set; }
             public string CustomerId { get; set; }
             public ICollection<OrderItem> OrderItems { get; set; }
             public User Customer { get; set; }
         }
  }
+
