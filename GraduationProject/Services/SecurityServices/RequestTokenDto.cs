e90a9eb49e9c703f99378ce55769d7c8fd87c8b0
ElzoheeryAhmed <elzoheeryahmed1807813@gmail.com>
2023-04-26 22:16:34 +0000

    Adding RequestTokenDto class

---
+++ GraduationProject/Services/SecurityServices/RequestTokenDto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Build.Framework;
+
+namespace GraduationProject.Services.SecurityServices
+{
+    public class RequestTokenDto
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+
+    }
+}
