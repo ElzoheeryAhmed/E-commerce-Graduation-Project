15c79ae627c62acf23bec31069ca28218189681b
ElzoheeryAhmed <elzoheeryahmed1807813@gmail.com>
2023-04-26 20:19:17 +0000

    Adding Register method definition

--- GraduationProject/Services/SecurityServices/IAuthService.cs
+++ GraduationProject/Services/SecurityServices/IAuthService.cs
@@ -1,6 +1,10 @@
 ﻿namespace GraduationProject.Services.SecurityServices
 {
+    //used for dependacy injection 
     public interface IAuthService
     {
+        //Register task
+        Task<AuthModel> RegisterAsync(UserRegisterDto dto);
+        
     }
 }
