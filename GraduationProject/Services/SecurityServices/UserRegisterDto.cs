e03762edcac504b1e3d61fd4da72b21cb7ae4c58
ElzoheeryAhmed <elzoheeryahmed1807813@gmail.com>
2023-04-26 22:15:04 +0000

    Update RegisterDto Identifying required fields for registeration

--- GraduationProject/Services/SecurityServices/UserRegisterDto.cs
+++ GraduationProject/Services/SecurityServices/UserRegisterDto.cs
@@ -1,18 +1,24 @@
-﻿namespace GraduationProject.Services.SecurityServices
+﻿using Microsoft.Build.Framework;
+
+namespace GraduationProject.Services.SecurityServices
 {
     public class UserRegisterDto
     {
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
-
+        [Required]
         public string UserName { get; set; }
+        [Required]
         public string Gender { get; set; }
+        [Required]
         public DateTime Birthdate { get; set; }
-
+        [Required]
         public string PhoneNumber { get; set; }
-
+        [Required]
         public string Email { get; set; }
-
+        [Required]
         public string Password { get; set; }
 
 
