1446e6375464c97dd9427771080951b45e41e29d
ElzoheeryAhmed <elzoheeryahmed1807813@gmail.com>
2023-04-26 20:30:43 +0000

    adding the model of the response returned to the user either successed or failed registration

---
+++ GraduationProject/Services/SecurityServices/AuthModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GraduationProject.Services.SecurityServices
+{
+    //returned object after login or register
+    public class AuthModel
+    {
+        public string Message { get; set; }
+        public bool IsAuthenticated { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public List<string> Roles { get; set; }  //IdentityUser allow multiple roles for specific user
+        public string Token { get; set; }
+        public DateTime ExpiresOn { get; set; }
+    }
+}
