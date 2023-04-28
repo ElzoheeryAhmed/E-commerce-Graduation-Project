32f11c7838ef816e88a8eba5ea85ea7ba65d2e65
ElzoheeryAhmed <elzoheeryahmed1807813@gmail.com>
2023-04-28 13:31:58 +0000

    .

--- GraduationProject/Models/User.cs
+++ GraduationProject/Models/User.cs
@@ -7,14 +7,14 @@ namespace GraduationProject.Models
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
 		public string Gender { get; set; }
-		public DateTime Birthdate { get; set; }
+        public DateTime Birthdate { get; set; }
 		
 		public ICollection<Rating> Ratings { get; set; } = new HashSet<Rating>();
 		public ICollection<Review> Reviews { get; set; } = new HashSet<Review>();
 		
 		public ICollection<Issue> Issues { get; set; }
-        	public ICollection<Order> Orders { get; set; }
-        	public ICollection<CartItem> CartItems { get; set; }
-        	public ICollection<WishlistItem> WishlistItems { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    	public ICollection<CartItem> CartItems { get; set; }
+    	public ICollection<WishlistItem> WishlistItems { get; set; }
 	}
 }
