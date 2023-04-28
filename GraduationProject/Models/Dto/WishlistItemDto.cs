9fd6fdb4c1a65d4f7aad6f36daf94d10035dc6e1
ElzoheeryAhmed <elzoheeryahmed1807813@gmail.com>
2023-04-28 13:24:23 +0000

    Adding wishlistItem Dtos

---
+++ GraduationProject/Models/Dto/WishlistItemDto.cs
@@ -0,0 +1,38 @@
+﻿namespace GraduationProject.Models.Dto
+{
+    public class WishlistItemDto
+    {
+        public string Name { get; set; }    
+        public string CustomerId { get; set; }
+
+        public string ProductId { get; set; }
+
+        public byte Quantity { get; set; }
+    }
+
+
+    public class WishlistItemDetailsDto
+    {
+
+        public string ProductId { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string HighResImageURLs { get; set; }
+
+        public byte Quantity { get; set; }
+    }
+
+
+    public class WishlistItemIdentifyDto
+    {
+        public string Name { get; set; }
+        public string CustomerId { get; set; }
+
+        public string ProductId { get; set; }
+
+    }
+
+}
