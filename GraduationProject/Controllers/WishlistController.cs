7df1d39fb62dd8608147c6513a30fc329e7007d6
ElzoheeryAhmed <elzoheeryahmed1807813@gmail.com>
2023-04-28 13:25:31 +0000

    Adding wishlist End Point

---
+++ GraduationProject/Controllers/WishlistController.cs
@@ -0,0 +1,151 @@
+﻿using System.Xml.Linq;
+using GraduationProject.Data;
+using GraduationProject.Models;
+using GraduationProject.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NetTopologySuite.Index.HPRtree;
+
+namespace GraduationProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WishlistController : ControllerBase
+    {
+
+        private AppDbContext _context;
+        public WishlistController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet(template:"GetAll/{id}")]
+        public async Task<IActionResult> GetAllasync(string id)
+        {
+            //Validation of CustomerId
+            var isValidCustomer = await _context.Users.AnyAsync(i => i.Id == id);
+            if (!isValidCustomer)
+            {
+                return BadRequest(error: "Invalid customer Id !");
+            }
+
+            var wishlistsNames = await _context.WishlistItems.Where(i => i.CustomerId == id).Select(n => new { n.Name }).Distinct().ToListAsync();
+            
+           return Ok(wishlistsNames);   
+        }
+
+        [HttpGet(template:"GetContent")]
+        public async Task<IActionResult> GetContentasync(string id,string name)
+        {
+            var wishlistItems = await _context.WishlistItems.Where(i => i.CustomerId == id && i.Name == name).Include(i => i.Product)
+                .Select(i => new WishlistItemDetailsDto {
+                    ProductId = i.ProductId,
+                    Title = i.Product.Title,
+                    HighResImageURLs = i.Product.HighResImageURLs,
+                    Price = i.Product.Price,
+                    Quantity=i.Quantity
+                })
+                .ToListAsync();
+
+            //Validation of customer wishlist combination
+            if (wishlistItems is null)
+            {
+                return NotFound();
+            }
+
+           
+
+            return Ok(wishlistItems);
+        }
+
+
+
+        [HttpPost]  
+        public async Task<IActionResult> AddItemAsync([FromBody] WishlistItemDto dto)
+        {
+
+            //Validation of CustomerId
+            var isValidCustomer = await _context.Users.AnyAsync(i => i.Id == dto.CustomerId);
+            if (!isValidCustomer)
+            {
+                return BadRequest(error: $"Invalid customer Id {dto.CustomerId} !");
+            }
+
+            //Validation of products Id
+            var isValidProduct = await _context.Products.AnyAsync(i => i.Id == dto.ProductId);
+            if (!isValidProduct)
+            {
+                return BadRequest(error: $"Invalid Product Id:{dto.ProductId}");
+            }
+            
+            var wishlistitem = new WishlistItem();
+
+            wishlistitem.Name = dto.Name;
+            wishlistitem.CustomerId = dto.CustomerId;
+            wishlistitem.ProductId= dto.ProductId;
+            wishlistitem.Quantity=dto.Quantity; 
+
+
+
+            await _context.WishlistItems.AddAsync(wishlistitem);
+            _context.SaveChanges();
+
+            return Ok(wishlistitem);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateQuantityAsync([FromBody] WishlistItemDto dto)
+        {
+            var wishlistItem = await _context.WishlistItems.FindAsync(dto.CustomerId, dto.ProductId, dto.Name);
+            
+            if(wishlistItem is null)
+            {
+                return NotFound();
+            }
+            //Enity framework by default tarck changes on entity no need for update
+            wishlistItem.Quantity = dto.Quantity;
+
+            _context.SaveChanges();
+
+            return Ok(wishlistItem);
+
+        }
+
+        [HttpDelete(template: "DeleteItem")]
+        public async Task<IActionResult> DeleteItemAsync([FromBody] WishlistItemIdentifyDto dto)
+        {
+            var wishlistItem = await _context.WishlistItems.FindAsync(dto.CustomerId, dto.ProductId, dto.Name);
+            
+            if(wishlistItem is null) {
+                return NotFound();
+            }
+
+            _context.WishlistItems.Remove(wishlistItem);
+            _context.SaveChanges(); 
+
+            return Ok(wishlistItem);    
+
+        }
+       
+        [HttpDelete(template: "DeleteWishlist")]
+        public async Task<IActionResult> DeleteWishlistAsync(string id, string name)
+        {
+            var wishlistItems = await _context.WishlistItems.Where(i => i.CustomerId == id && i.Name == name)
+                .ToListAsync();
+            
+            //Validation of customer wishlist combination
+            if (wishlistItems is null)
+            {
+                return NotFound();
+            }
+
+            _context.WishlistItems.RemoveRange(wishlistItems);
+            _context.SaveChanges();
+
+            return Ok(wishlistItems);   
+
+        }
+
+    }
+}
