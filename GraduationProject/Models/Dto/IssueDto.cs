543558256f99de6593431d2ee5b26a5496b4c65a
ElzoheeryAhmed <elzoheeryahmed1807813@gmail.com>
2023-04-29 12:44:28 +0000

    Adding Issue Dtos

---
+++ GraduationProject/Models/Dto/IssueDto.cs
@@ -0,0 +1,32 @@
+﻿namespace GraduationProject.Models.Dto
+{
+
+    
+    public class UnresponedIssueDto
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+
+        public DateTime SubmitDate { get; set; }
+    }
+
+    public class responedIssueDto: UnresponedIssueDto
+    {
+        public string Response { get; set; }
+
+        public DateTime? RespondDate { get; set; }
+        
+
+        public string AdminId { get; set; }
+    }
+
+    public class responseIssueDto 
+    {
+        public int IssueId { get; set; }
+
+        public string Response { get; set; }
+
+        public string AdminId { get; set; }
+    }
+
+}
