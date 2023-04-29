8f74331aeedc46e505391e49f5b85295fe61cac0
ElzoheeryAhmed <elzoheeryahmed1807813@gmail.com>
2023-04-29 12:42:54 +0000

    Adding issue entity updates migration

---
+++ GraduationProject/Migrations/20230429121547_Issue-ResponseNStatusUpdates.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GraduationProject.Migrations
+{
+    public partial class IssueResponseNStatusUpdates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           
+
+                migrationBuilder.AddColumn<string>(
+                name: "Response",
+                table: "Issues",
+                type: "nvarchar(max)",
+                nullable: true);
+           
+           
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+
+            migrationBuilder.DropColumn(
+                name: "Response",
+                table: "Issues");
+
+            
+        }
+    }
+}
