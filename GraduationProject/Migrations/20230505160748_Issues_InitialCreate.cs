de1d26e16d720f68a76e3b505fbda0b6e761932f
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-05-05 22:50:27 +0300

    Update and rename 20230408214841_Issue-InitialCreate.cs to 20230505160748_Issues_InitialCreate.cs

--- GraduationProject/Migrations/20230505160748_Issues_InitialCreate.cs
+++ GraduationProject/Migrations/20230505160748_Issues_InitialCreate.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace GraduationProject.Migrations
 {
-    public partial class IssueInitialCreate : Migration
+    public partial class Issues_InitialCreate : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -16,6 +16,7 @@ namespace GraduationProject.Migrations
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Response = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     SubmitDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
                     RespondDate = table.Column<DateTime>(type: "datetime2", nullable: true),
                     Status = table.Column<string>(type: "varchar(15)", nullable: false, defaultValue: "Submitted"),
