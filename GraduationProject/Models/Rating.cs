c6a4f36d8d690cfd5c06514a2ada19d7e79bad26
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-05-05 21:14:38 +0300

    Update Rating.cs

--- GraduationProject/Models/Rating.cs
+++ GraduationProject/Models/Rating.cs
@@ -6,8 +6,7 @@ namespace GraduationProject.Models
     /// <summary>
     /// This class defines a many-to-many relationship between the User and Product entities. The Rating entity acts as the join table that represents the ratings given by users to products.
     /// </summary>
-    public class Rating
-    {
+    public class Rating {
         public string UserId { get; set; }
         public User User { get; set; }
         
