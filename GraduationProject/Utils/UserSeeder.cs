ef4d10f8e5b50b54787f2c994bcc49219085196c
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-05-05 21:10:09 +0300

    Update UserSeeder.cs

--- GraduationProject/Utils/UserSeeder.cs
+++ GraduationProject/Utils/UserSeeder.cs
@@ -1,13 +1,10 @@
-using GraduationProject.Models;
 using GraduationProject.Models.Dto;
 using Newtonsoft.Json;
 
 namespace GraduationProject.Utils
 {
-	public static class UserSeeder
-	{
-		public static List<UserCreateDto> Seed(int file_index)
-		{
+	public static class UserSeeder {
+		public static List<UserCreateDto> Seed(int file_index) {
 			string jsonFilePath = Path.Combine(Environment.CurrentDirectory,
 									"Data",
 									"InitialSeed",
@@ -18,15 +15,12 @@ namespace GraduationProject.Utils
 			List<UserCreateDto> users = new List<UserCreateDto>();
 			
 			// Parsing the json file and creating the appropriate objects.
-			using (StreamReader r = new StreamReader(jsonFilePath))
-			{
+			using (StreamReader r = new StreamReader(jsonFilePath)) {
 				// Parse the JSON into a dynamic object.
 				dynamic extractedUsers = JsonConvert.DeserializeObject(r.ReadToEnd());
 				
-
 				// int counter = 0;
-				foreach (var user in extractedUsers)
-				{
+				foreach (var user in extractedUsers) {
 					string id = user.Value.id;
 					string firstName = user.Value.first_name;
 					string lastName = user.Value.last_name;
@@ -35,11 +29,10 @@ namespace GraduationProject.Utils
 					string gender = user.Value.gender == 1 ? "Male" : "Female";
 					string email = user.Value.email;
 					string phone = user.Value.phone;
-					// Console.WriteLine($"Id:\t\t{id}\nFirst Name:\t{firstName}\nLast Name:\t{lastName}\nBirthdate:\t{birthdate}\nGender:\t\t{gender}\nEmail:\t\t{email}\nPhone:\t\t{phone}\n");
-
+					
 					UserCreateDto newUser = new UserCreateDto { Id = id, FirstName = firstName, LastName = lastName, UserName = username, Birthdate = DateTime.Parse(birthdate), Gender = gender, Email = email, PhoneNumber = phone };
 					users.Add(newUser);
-
+					
 					// // Below code is for debugging.
 					// if (counter++ >= 10)
 					// 	break;
