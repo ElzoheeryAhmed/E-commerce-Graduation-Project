94ef40b0375a6b9bc6acd98896211d31dcf4cc48
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-05-05 23:16:08 +0300

    Adding swagger documentation.

--- GraduationProject/Controllers/BrandController.cs
+++ GraduationProject/Controllers/BrandController.cs
@@ -1,8 +1,8 @@
+using System.Linq.Expressions;
 using AutoMapper;
+using GraduationProject.Controllers.FilterParameters;
 using GraduationProject.IRepository;
 using GraduationProject.Models;
-using GraduationProject.Models.Dto;
-using GraduationProject.Repository;
 using GraduationProject.Repository.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -24,18 +24,27 @@ namespace GraduationProject.Controllers
 			_mapper = mapper;
 		}
         
-        /*[HttpGet]
+		/// <summary>
+		/// Get a page of brands.
+		/// </summary>
+		/// <param name="pagingFilter">The paging filter.</param>
+		/// <param name="orderBy">The order by.</param>
+		/// <returns></returns>
+		/// <response code="200">Returns a page of brands.</response>
+		/// <response code="500">If an error occurs.</response>
+        [HttpGet]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 		[Route("Brands")]
 		public async Task<IActionResult> GetBrands([FromQuery] Categories_BrandsPagingFilter pagingFilter, [FromQuery] string? orderBy = null) {
 			try {
-				var brands = await _unitOfWork.Brands.GetAllAsync(null, pagingFilter, true, orderBy: orderBy);
+				EntityFieldsFilter fieldsFilters = new EntityFieldsFilter() { OnlySelectFields = "Id,Name",  FieldsToExclude = "Products" };
+				Expression<Func<Brand, Brand>> selectExpression = QueryableExtensions<Brand>.EntityFieldsSelector(fieldsFilters);
+				
+				var brands = await _unitOfWork.Brands.GetAllAsync(null, pagingFilter, true, selectExpression: selectExpression, orderBy: orderBy);
 				
 				var json = JsonConvert.SerializeObject(brands, Formatting.Indented,
-					new GenericFieldBasedJsonConverter<Brand>(
-						new HashSet<string>(typeof(Brand).GetProperties().Select(p => p.Name)),
-						null, "Products"));
+					new GenericFieldBasedJsonConverter<Brand>(fieldsFilters.OnlySelectFields.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToHashSet(), fieldsFilters));
 				
 				return Ok(json);
 			}
@@ -45,8 +54,16 @@ namespace GraduationProject.Controllers
 				return StatusCode(500, "Internal Server Error. Something went wrong when trying to access the brands data.");
 			}
 		}
-		*/
-        /*
+
+        
+		/// <summary>
+		/// Get a brand by id.
+		/// </summary>
+		/// <param name="id">The brand id.</param>
+		/// <returns></returns>
+		/// <response code="200">Returns the brand with the specified id.</response>
+		/// <response code="404">If the brand with the specified id is not found.</response>
+		/// <response code="500">If an error occurs.</response>
         [HttpGet("{id}")]
 		[ActionName(nameof(GetBrand))]
 		[ProducesResponseType(StatusCodes.Status200OK)]
@@ -54,7 +71,10 @@ namespace GraduationProject.Controllers
 		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 		public async Task<IActionResult> GetBrand(int id) {
 			try {
-				var brand = await _unitOfWork.Brands.GetByAsync(p => p.Id == id, true);
+				EntityFieldsFilter fieldsFilters = new EntityFieldsFilter() { OnlySelectFields = "Id,Name",  FieldsToExclude = "Products" };
+				Expression<Func<Brand, Brand>> selectExpression = QueryableExtensions<Brand>.EntityFieldsSelector(fieldsFilters);
+				
+				var brand = await _unitOfWork.Brands.GetByAsync(p => p.Id == id, true, selectExpression: selectExpression);
 				
 				if (brand == null) {
 					_logger.LogInformation($"Failed GET attempt in {nameof(GetBrand)}. Could not find a brand with id={id}.");
@@ -63,14 +83,14 @@ namespace GraduationProject.Controllers
 				}
 				
 				var json = JsonConvert.SerializeObject(brand, Formatting.Indented,
-					new GenericFieldBasedJsonConverter<Brand>(_fields, fieldsToExclude: "Products"));
+					new GenericFieldBasedJsonConverter<Brand>(fieldsFilters.OnlySelectFields.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToHashSet(), fieldsFilters));
 				
 				return Ok(json);
 			}
 			catch (Exception ex) {
-				_logger.LogError(ex, $"Something went wrong when trying to access the data of the product with id={id}.");
-				return StatusCode(500, $"Internal Server Error. Something went wrong when trying to access the data of the product with id={id}.");
+				_logger.LogError(ex, $"Something went wrong when trying to access the brand with id={id}.");
+				return StatusCode(500, $"Internal Server Error. Something went wrong when trying to access the brand with id={id}.");
 			}
-		}*/
+		}
     }
 }
