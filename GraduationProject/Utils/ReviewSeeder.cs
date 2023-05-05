29152529fd63f3ff6206e1b4f7c8c9516c398ed4
Ahmed Tarek <70451237+Ryen-042@users.noreply.github.com>
2023-05-05 21:11:49 +0300

    Update ReviewSeeder.cs

--- GraduationProject/Utils/ReviewSeeder.cs
+++ GraduationProject/Utils/ReviewSeeder.cs
@@ -4,27 +4,25 @@ using Newtonsoft.Json;
 
 namespace GraduationProject.Utils
 {
-    public class ReviewSeeder
-    {
-        public static List<ReviewDto> Seed(int fileSelector=0)
-		{
+    public class ReviewSeeder {
+        public static List<ReviewDto> Seed(int fileSelector=0) {
             string jsonFilePath;
-			if (fileSelector == 0)
-			{
+			if (fileSelector == 0) {
 				jsonFilePath = Path.Combine(Environment.CurrentDirectory,
 											"Data",
 											"InitialSeed",
-											"ClothingReviews.json");
-			} else {
+											"clothes_review_with_sentiment_scores.json");
+			}
+			
+			else {
                 jsonFilePath = Path.Combine(Environment.CurrentDirectory,
 											"Data",
 											"InitialSeed",
-											"Home_KitchenReviews.json");
+											"home_kitchen_reviews_with_sentiment_scores.json");
             }
             
 			// Parsing the json file and creating the appropriate objects.
-			using (StreamReader r = new StreamReader(jsonFilePath))
-			{
+			using (StreamReader r = new StreamReader(jsonFilePath)) {
 				// Parse the JSON into a dynamic object.
 				Dictionary<string, Dictionary<string, string>> extractedReviews = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(r.ReadToEnd());
 
@@ -32,8 +30,7 @@ namespace GraduationProject.Utils
 				List<ReviewDto> reviews = new List<ReviewDto>();
 
 				// int counter = 0;
-				foreach (var key in extractedReviews["reviewerID"])
-				{
+				foreach (var key in extractedReviews["reviewerID"]) {
 					// Parsing the inconsistent reviewDates.
 					// DateTime reviewDate = DateTime.ParseExact(extractedReviews["reviewTime"][key.Key], "MM dd, yyyy", CultureInfo.InvariantCulture);
 					DateTime reviewDate;
@@ -43,12 +40,12 @@ namespace GraduationProject.Utils
 											formats, CultureInfo.InvariantCulture, DateTimeStyles.None, out reviewDate);
 					
 					// Creating a Rating object.
-					ReviewDto review = new ReviewDto
-					{
+					ReviewDto review = new ReviewDto {
 						UserId = key.Value.ToString(),
 						ProductId = extractedReviews["asin"][key.Key].ToString().Trim(),
 						ReviewText = extractedReviews["reviewText"][key.Key] ?? "",
 						Timestamp = reviewDate,
+						SentimentScore = int.Parse(extractedReviews["sentiment"][key.Key])
 					};
 
 					reviews.Add(review);
