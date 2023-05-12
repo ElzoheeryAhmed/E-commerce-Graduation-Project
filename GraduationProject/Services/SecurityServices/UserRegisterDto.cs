c69d0e071395a340d1516ce34282c934ee5b1f46
ElzoheeryAhmed <elzoheeryahmed1807813@gmail.com>
2023-05-12 14:24:30 +0000

    Constraining UserRegisterDto

--- GraduationProject/Services/SecurityServices/UserRegisterDto.cs
+++ GraduationProject/Services/SecurityServices/UserRegisterDto.cs
@@ -1,5 +1,6 @@
 ﻿//using Microsoft.Build.Framework;
 
+using GraduationProject.Models;
 using System.ComponentModel.DataAnnotations;
 
 namespace GraduationProject.Services.SecurityServices
@@ -7,19 +8,38 @@ namespace GraduationProject.Services.SecurityServices
     public class UserRegisterDto
     {
         [Required]
+        [StringLength(15,MinimumLength=3)]
         public string FirstName { get; set; }
+
+
         [Required]
+        [StringLength(15, MinimumLength = 3)]
         public string LastName { get; set; }
+
+
         [Required]
+        [StringLength(15, MinimumLength = 3)]
         public string UserName { get; set; }
+
         [Required]
-        public string Gender { get; set; }
+        [EnumDataType(typeof(Gender),ErrorMessage="Enter:0 for Male, and 1 for Female")]
+        public Gender  Gender { get; set; }
+
+
         [Required]
+        //[Range(typeof(DateTime),(DateTime.Now.AddYears(-15)).ToString("yyyy-MM-dd"), DateTime.Now.AddYears(-100).ToString("yyyy-MM-dd"))]
         public DateTime Birthdate { get; set; }
+
+
         [Required]
         public string PhoneNumber { get; set; }
+
+
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+
         [Required]
         public string Password { get; set; }
 
