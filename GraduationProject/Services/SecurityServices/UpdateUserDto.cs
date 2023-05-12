ec7596c764987847c4e42e5a891e3d5ddf41ae5f
ElzoheeryAhmed <elzoheeryahmed1807813@gmail.com>
2023-05-12 14:32:58 +0000

    Adding UpdateUserDto

---
+++ GraduationProject/Services/SecurityServices/UpdateUserDto.cs
@@ -0,0 +1,34 @@
+﻿using GraduationProject.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace GraduationProject.Services.SecurityServices
+{
+    public class UpdateUserDto
+    {
+        [StringLength(15, MinimumLength = 3)]
+        public string FirstName { get; set; }
+
+
+        [StringLength(15, MinimumLength = 3)]
+        public string LastName { get; set; }
+
+
+        [StringLength(15, MinimumLength = 3)]
+        public string UserName { get; set; }
+
+        [EnumDataType(typeof(Gender), ErrorMessage = "Enter:0 for Male, and 1 for Female")]
+        public Gender Gender { get; set; }
+
+
+        //[Range(typeof(DateTime),(DateTime.Now.AddYears(-15)).ToString("yyyy-MM-dd"), DateTime.Now.AddYears(-100).ToString("yyyy-MM-dd"))]
+        public DateTime Birthdate { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+
+    }
+}
