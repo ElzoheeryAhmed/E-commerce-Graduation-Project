86aa16c6deec5f5a11ab48c3876b214d2c324ce6
ElzoheeryAhmed <elzoheeryahmed1807813@gmail.com>
2023-06-14 13:03:08 +0000

    Adjusting  constraints of RequestTokenDto

--- GraduationProject/Services/SecurityServices/RequestTokenDto.cs
+++ GraduationProject/Services/SecurityServices/RequestTokenDto.cs
@@ -10,6 +10,7 @@ namespace GraduationProject.Services.SecurityServices
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+        
         [Required]
         public string Password { get; set; }
 
