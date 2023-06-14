6272739f9416a341f6906be2bdf0b9ecc74afbdd
ElzoheeryAhmed <elzoheeryahmed1807813@gmail.com>
2023-06-14 13:00:00 +0000

    Adding the alterModel class && error parsing helper

---
+++ GraduationProject/Services/SecurityServices/SecurityHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GraduationProject.Services.SecurityServices
+{
+    public class SecurityHelper
+    {
+        public static string parseError(IEnumerable<IdentityError> Errors) 
+        {
+            var errors = string.Empty;
+
+            foreach (var error in Errors)
+                errors += $"{error.Description},";
+
+            return  errors ;
+        }
+    }
+}
