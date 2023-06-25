86b0c37b33ed8358d5bf1626adf1d8f58e2f7a71
unknown <elzoheeryahmed1807813@gmail.com>
2023-06-25 07:07:14 +0000

    calling regular chatbot model

---
+++ GraduationProject/Chatbot/regularchatbotresponses.js
@@ -0,0 +1,27 @@
+﻿async function getBotResponse(input) {
+    let  x = "";
+    await fetch('http://18.159.111.193/api/Review/chatbots/regular/'+input)
+        .then(response => response.text())
+        .then(text => {  x= text ; });
+
+    return x;
+    //rock paper scissors
+    if (input == "rock") {
+        return "paper";
+    } else if (input == "paper") {
+        return "scissors";
+    } else if (input == "scissors") {
+        return "rock";
+    }
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else!";
+    }
+
+}
+
