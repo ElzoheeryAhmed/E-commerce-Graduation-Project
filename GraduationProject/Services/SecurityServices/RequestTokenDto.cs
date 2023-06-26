eb9897e775a8739b40f9a2c2191a14722e3ce35d
ElzoheeryAhmed <elzoheeryahmed1807813@gmail.com>
2023-06-26 14:32:59 +0000

    Updating RequestTokenDto

--- GraduationProject/Services/SecurityServices/RequestTokenDto.cs
+++ GraduationProject/Services/SecurityServices/RequestTokenDto.cs
@@ -6,11 +6,13 @@ using System.ComponentModel.DataAnnotations;
 namespace GraduationProject.Services.SecurityServices
 {
     public class RequestTokenDto
-    {
+    { 
+        ///  <value> Email address </value>
         [Required]
         [EmailAddress]
         public string Email { get; set; }
-        
+
+        ///  <value> Password </value>
         [Required]
         public string Password { get; set; }
 
